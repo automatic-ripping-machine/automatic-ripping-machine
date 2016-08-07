13116c3cab6aebb39c4fa1e4f6084b0d3542460d
Benjamin Bryan <ben@b3n.org>
2016-08-06 23:38:25 -0700

    fix for "Audio CDs" in iso9660 format like LOTR

--- identify.sh
+++ identify.sh
@@ -12,14 +12,14 @@ if [ $ID_FS_TYPE == "udf" ]; then
 	echo "ripping video" >> /opt/arm/log
 	/opt/arm/video_rip.sh
 
+elif (($ID_CDROM_MEDIA_TRACK_COUNT_AUDIO > 0 )); then
+	echo "identified audio" >> /opt/arm/log
+	abcde
 
 elif [ $ID_FS_TYPE == "iso9660" ]; then
 	echo "identified data" >> /opt/arm/log
 	/opt/arm/data_rip.sh
 	eject
-elif (($ID_CDROM_MEDIA_TRACK_COUNT_AUDIO > 0 )); then
-	echo "identified audio" >> /opt/arm/log
-	abcde
 else
 	echo "unable to identify" >> /opt/arm/log
 	echo $ID_CDROM_MEDIA_TRACK_COUNT_AUDIO >> /opt/arm/log
