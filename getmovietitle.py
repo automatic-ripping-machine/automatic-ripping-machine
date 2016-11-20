5466d33e24cd82d3d608eb63779b4a8fed1b221f
Benjamin Bryan <ben@b3n.org>
2016-11-19 21:27:43 -0800

    added bluray

--- getmovietitle.py
+++ getmovietitle.py
@@ -5,17 +5,46 @@ import pydvdid
 import urllib
 # import libxml2
 import xmltodict
+import os
+import datetime
+import unicodedata
 
 parser = argparse.ArgumentParser(description='hello')
 parser.add_argument('-p','--path', help='Mount path to disc',required=True)
 args = parser.parse_args()
+disc_title = ""
 
-crc64=pydvdid.compute(args.path)
-dvd_info_xml = urllib.urlopen("http://metaservices.windowsmedia.com/pas_dvd_B/template/GetMDRDVDByCRC.xml?CRC={0}".format(crc64)).read()
+def getdvdtitle():
+    crc64=pydvdid.compute(args.path)
+    print crc64
+    dvd_info_xml = urllib.urlopen("http://metaservices.windowsmedia.com/pas_dvd_B/template/GetMDRDVDByCRC.xml?CRC={0}".format(crc64)).read()
+    
+    doc = xmltodict.parse(dvd_info_xml)
+    dvd_title = doc['METADATA']['MDR-DVD']['dvdTitle']
+    dvd_release_date = doc['METADATA']['MDR-DVD']['releaseDate']
 
-doc = xmltodict.parse(dvd_info_xml)
-dvd_title = doc['METADATA']['MDR-DVD']['dvdTitle']
-dvd_release_date = doc['METADATA']['MDR-DVD']['releaseDate']
+    # title + release year
+    return dvd_title + " (" + dvd_release_date.split()[0] + ")"
 
-# title + release year
-print dvd_title + " (" + dvd_release_date.split()[0] + ")"
+def getbluraytitle():
+    with open(args.path + '/BDMV/META/DL/bdmt_eng.xml') as fd:
+        doc = xmltodict.parse(fd.read())
+
+    bluray_title = doc['disclib']['di:discinfo']['di:title']['di:name']
+
+    bluray_modified_timestamp = os.path.getmtime(args.path + '/BDMV/META/DL/bdmt_eng.xml')
+    bluray_year = (datetime.datetime.fromtimestamp(bluray_modified_timestamp).strftime('%Y'))
+
+    bluray_title = unicodedata.normalize('NFKD', bluray_title).encode('ascii','ignore')
+
+    bluray_title.replace(' - Blu-ray','')
+    bluray_title = bluray_title.replace(' - Blu-rayTM','')
+    return bluray_title + " (" + bluray_year + ")"
+
+try:
+    disc_title = getdvdtitle()
+except:
+    disc_title = getbluraytitle()
+    print disc_title
+else:
+    print disc_title
