f2a423d4eb7c39eae206086a6612816eaa714213
Benjamin Bryan <ben@b3n.org>
2016-11-20 07:48:44 -0800

    clean up

--- getmovietitle.py
+++ getmovietitle.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
 
 import argparse
-import pydvdid
-import urllib
-# import libxml2
-import xmltodict
+import urllib.request
 import os
 import datetime
+import pydvdid
 import unicodedata
+import xmltodict
 
-parser = argparse.ArgumentParser(description='hello')
-parser.add_argument('-p','--path', help='Mount path to disc',required=True)
-args = parser.parse_args()
-disc_title = ""
+def entry():
+    """ Entry to program, parses arguments"""
+    parser = argparse.ArgumentParser(description='hello')
+    parser.add_argument('-p', '--path', help='Mount path to disc', required=True)
+
+    return parser.parse_args()
 
 def getdvdtitle():
-    crc64=pydvdid.compute(args.path)
-    print crc64
-    dvd_info_xml = urllib.urlopen("http://metaservices.windowsmedia.com/pas_dvd_B/template/GetMDRDVDByCRC.xml?CRC={0}".format(crc64)).read()
-    
+    """ Calculates CRC64 for the DVD and calls Microsoft's
+        Metaservices and returns the Title and year of DVD """
+    crc64 = pydvdid.compute(args.path)
+    # print (crc64)
+    dvd_info_xml = urllib.request.urlopen(
+        "http://metaservices.windowsmedia.com/pas_dvd_B/template/GetMDRDVDByCRC.xml?CRC={0}".
+        format(crc64)).read()
+
     doc = xmltodict.parse(dvd_info_xml)
     dvd_title = doc['METADATA']['MDR-DVD']['dvdTitle']
     dvd_release_date = doc['METADATA']['MDR-DVD']['releaseDate']
@@ -27,24 +32,29 @@ def getdvdtitle():
     return dvd_title + " (" + dvd_release_date.split()[0] + ")"
 
 def getbluraytitle():
-    with open(args.path + '/BDMV/META/DL/bdmt_eng.xml') as fd:
-        doc = xmltodict.parse(fd.read())
+    """ Get's Blu-Ray title by parsing XML in bdmt_eng.xml """
+    with open(args.path + '/BDMV/META/DL/bdmt_eng.xml') as xml_file:
+        doc = xmltodict.parse(xml_file.read())
 
     bluray_title = doc['disclib']['di:discinfo']['di:title']['di:name']
 
     bluray_modified_timestamp = os.path.getmtime(args.path + '/BDMV/META/DL/bdmt_eng.xml')
     bluray_year = (datetime.datetime.fromtimestamp(bluray_modified_timestamp).strftime('%Y'))
 
-    bluray_title = unicodedata.normalize('NFKD', bluray_title).encode('ascii','ignore')
+    bluray_title = unicodedata.normalize('NFKD', bluray_title).encode('ascii', 'ignore')
 
-    bluray_title.replace(' - Blu-ray','')
-    bluray_title = bluray_title.replace(' - Blu-rayTM','')
+    bluray_title = bluray_title.replace(' - Blu-ray', '')
+    bluray_title = bluray_title.replace(' - Blu-rayTM', '')
     return bluray_title + " (" + bluray_year + ")"
+
+#pylint: disable=C0103
+
+args = entry()
 
 try:
     disc_title = getdvdtitle()
 except:
     disc_title = getbluraytitle()
-    print disc_title
+    print(disc_title)
 else:
-    print disc_title
+    print(disc_title)
