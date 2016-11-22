3e214abe2ec72e497d97ed896de3f307dc83d3e6
Benjamin Bryan <ben@b3n.org>
2016-11-21 21:19:59 -0800

    Rip option using backup method in MakeMKV and main-feature in HandBrake (#6) (#8)
    * Add ability to rip using backup method in MakeMKV and main-feature in Handbrake
    * Added logic for when backup method is selected but all files wanted.

--- video_transcode.sh
+++ video_transcode.sh
@@ -8,34 +8,50 @@ LABEL=$2
 TIMESTAMP=$3
 TRANSSTART=$(date +%s);
 
+
 echo "Start video transcoding script" >> $LOG
 
 	DEST=${ARMPATH}/${LABEL}_${TIMESTAMP}
 	mkdir $DEST
 
-        for FILE in `ls $SRC`
-                do
-                filename=$(basename $FILE)
-                extension=${filename##*.}
-                filename=${filename%.*}
-	
-		echo "Transcoding file $FILE" >> $LOG
+	if [ $RIPMETHOD = "backup" ] && [ "$MAINFEATURE" = true ] && [ $ID_CDROM_MEDIA_BD = "1" ]; then
+		echo "Transcoding BluRay main feature only." >> $LOG
+		$HANDBRAKE_CLI -i $SRC -o $DEST/$LABEL.$DEST_EXT --main-feature --preset="$HB_PRESET" --subtitle scan -F 2>> $LOG
+		rmdir -rf $SRC
+	elif [ $RIPMETHOD = "backup" ] && [ "$MAINFEATURE" = false ] && [ $ID_CDROM_MEDIA_BD = "1" ]; then
+		echo "Transcoding BluRay all titles above minlength." >> $LOG
+		$HANDBRAKE_CLI -i $SRC -o $DEST/$LABEL.$DEST_EXT --min-duration $MINLENGTH --preset="$HB_PRESET" --subtitle scan -F 2>> $LOG
+		rmdir -rf $SRC
+	elif [ $MAINFEATURE = true ] && [ $ID_CDROM_MEDIA_DVD = "1" ]; then
+		echo "Transcoding DVD main feature only." >> $LOG
+                $HANDBRAKE_CLI -i $DEVNAME -o $DEST/$LABEL.$DEST_EXT --main-feature --preset="$HB_PRESET" --subtitle scan -F 2>> $LOG
+		eject $DEVNAME
+		rmdir $SRC
+	else
+		echo "Transcoding all files." >> $LOG
+	        for FILE in `ls $SRC`
+                	do
+                	filename=$(basename $FILE)
+                	extension=${filename##*.}
+                	filename=${filename%.*}
 
-                $HANDBRAKE_CLI -i $SRC/$FILE -o $DEST/$filename.$DEST_EXT --preset="$HB_PRESET" --subtitle scan -F 2>> $LOG
-		#TIMESTAMP=`date '+%Y_%m_%d__%H_%M_%S'`;
-		#mv $SRC/$FILE $SRC/done/$TIMESTAMP.$FILE
-		#mv $DEST/$FILE $DEST/done/$FILE
-		rm $SRC/$FILE
+			echo "Transcoding file $FILE" >> $LOG
 
-       	done
+                	$HANDBRAKE_CLI -i $SRC/$FILE -o $DEST/$filename.$DEST_EXT --preset="$HB_PRESET" --subtitle scan -F 2>> $LOG
+			rm $SRC/$FILE
+       		done
+		rmdir $SRC
+	fi
 
-rmdir $SRC
+#rmdir $SRC
 
 TRANSEND=$(date +%s);
 TRANSSEC=$(($TRANSEND-$TRANSSTART));
 TRANSTIME="$(($TRANSSEC / 3600)) hours, $((($TRANSSEC / 60) % 60)) minutes and $(($TRANSSEC % 60)) seconds."
 
 echo "STAT: ${ID_FS_LABEL} transcoded in ${TRANSTIME}" >> $LOG
+
+#echo /opt/arm/rename.sh $DEST
 
 echo /opt/arm/notify.sh "\"Transcode: ${ID_FS_LABEL} completed in ${TRANSTIME}\"" |at now
 
