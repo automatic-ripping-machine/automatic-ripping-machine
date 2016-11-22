797f32bf500ac62af0ab928001b543cd5ec891d0
Benjamin Bryan <ben@b3n.org>
2016-11-21 22:39:11 -0800

    param devname

--- identify.sh
+++ identify.sh
@@ -33,7 +33,7 @@ if [ $ID_FS_TYPE == "udf" ]; then
 
 			if [ $GET_VIDEO_TITLE == true ]; then
 
-				GET_TITLE_OUTPUT=$(./getmovietitle.py -p /mnt/dev/sr0 2>&1)
+				GET_TITLE_OUTPUT=$(./getmovietitle.py -p /mnt/${DEVNAME} 2>&1)
 				GET_TITLE_RESULT=$?
 
 				if [ $GET_TITLE_RESULT = 0 ]; then
