c0dab602a6b31794bfbca18f223bc65231261099
Benjamin Bryan <ben@b3n.org>
2016-11-28 21:31:21 -0800

    shellcheck

--- data_rip.sh
+++ data_rip.sh
@@ -7,16 +7,16 @@ source /opt/arm/config
 
 
 
-        TIMESTAMP=`date '+%Y%m%d_%H%M%S'`;
-        DEST=/mnt/media/ARM/${TIMESTAMP}_${ID_FS_LABEL}
-        mkdir $DEST
+        TIMESTAMP=$(date '+%Y%m%d_%H%M%S');
+        DEST="/mnt/media/ARM/${TIMESTAMP}_${ID_FS_LABEL}"
+        mkdir "$DEST"
 	FILENAME=${ID_FS_LABEL}_disc.iso
 
 
 	#dd if=/dev/sr0 of=$DEST/$FILENAME 
-	cat /dev/sr0 > $DEST/$FILENAME
+	cat "$DEVNAME" > "$DEST/$FILENAME"
 
 	eject
 
 
-} >> $LOG
+} >> "$LOG"
--- identify.sh
+++ identify.sh
@@ -3,38 +3,38 @@
 source /opt/arm/config
 
 # Create log dir if needed
-mkdir -p $LOGPATH
+mkdir -p "$LOGPATH"
 
 {
-echo "Starting Identify Script..." >> $LOG
+echo "Starting Identify Script..." >> "$LOG"
 
 VIDEO_TITLE=""
 HAS_NICE_TITLE=""
 
 
 #Clean up old log files
-FILESFOUND=( $(find $LOGPATH -mtime +$LOGLIFE -type f))
-echo "Deleting ${#FILESFOUND[@]} old log files:"${FILESFOUND[@]} >> $LOG
-find $LOGPATH -mtime +$LOGLIFE -type f -delete
+FILESFOUND=( $(find "$LOGPATH" -mtime +"$LOGLIFE" -type f))
+echo "Deleting ${#FILESFOUND[@]} old log files: ${FILESFOUND[*]}" >> "$LOG"
+find "$LOGPATH" -mtime +"$LOGLIFE" -type f -delete
 
 # Set Home to home folder of user that is setup to run MakeMKV
 export HOME="/root/"
 
 # Output UDEV info
-udevadm info -q env -n ${DEVNAME} >> $LOG
+udevadm info -q env -n "$DEVNAME" >> "$LOG"
 
-if [ $ID_FS_TYPE == "udf" ]; then
-	echo "identified udf" >> $LOG
-	echo "found ${ID_FS_LABEL} on ${DEVNAME}" >> $LOG
+if [ "$ID_FS_TYPE" == "udf" ]; then
+	echo "identified udf" >> "$LOG"
+	echo "found ${ID_FS_LABEL} on ${DEVNAME}" >> "$LOG"
 
-	if [ $ARM_CHECK_UDF == true ]; then
+	if [ "$ARM_CHECK_UDF" == true ]; then
 		# check to see if this is really a video
-		mkdir -p /mnt/${DEVNAME}
-		mount ${DEVNAME} /mnt/${DEVNAME}
+		mkdir -p "/mnt/$DEVNAME"
+		mount "$DEVNAME" "/mnt/DEVNAME"
 		if [[ -d /mnt/${DEVNAME}/VIDEO_TS || -d /mnt/${DEVNAME}/BDMV ]]; then
-			echo "identified udf as video" >> $LOG
+			echo "identified udf as video" >> "$LOG"
 
-			if [ $GET_VIDEO_TITLE == true ]; then
+			if [ "$GET_VIDEO_TITLE" == true ]; then
 
 				GET_TITLE_OUTPUT=$(/opt/arm/getmovietitle.py -p "/mnt${DEVNAME}" 2>&1)
 				GET_TITLE_RESULT=$?
@@ -65,35 +65,35 @@ if [ $ID_FS_TYPE == "udf" ]; then
 			echo "video title is now ${VIDEO_TITLE}"
 			echo "video type is ${VIDEO_TYPE}"
 
-			umount /mnt/${DEVNAME}
-			/opt/arm/video_rip.sh "$VIDEO_TITLE" "$HAS_NICE_TITLE" "$VIDEO_TYPE" $LOG
+			umount "/mnt/$DEVNAME"
+			/opt/arm/video_rip.sh "$VIDEO_TITLE" "$HAS_NICE_TITLE" "$VIDEO_TYPE" "$LOG"
 		else
-			umount /mnt/${DEVNAME}
-			echo "identified udf as data" >> $LOG
-			/opt/arm/data_rip.sh $LOG
-			eject $DEVNAME
+			umount "/mnt/$DEVNAME"
+			echo "identified udf as data" >> "$LOG"
+			/opt/arm/data_rip.sh "$LOG"
+			eject "$DEVNAME"
 
 		fi
 	else
-		echo "ARM_CHECK_UDF is false, assuming udf is video" >> $LOG
-		/opt/arm/video_rip.sh $LOG
+		echo "ARM_CHECK_UDF is false, assuming udf is video" >> "$LOG"
+		/opt/arm/video_rip.sh "$LOG"
 	fi	
 
 
-elif (($ID_CDROM_MEDIA_TRACK_COUNT_AUDIO > 0 )); then
-	echo "identified audio" >> $LOG
-	abcde -d $DEVNAME
+elif (("$ID_CDROM_MEDIA_TRACK_COUNT_AUDIO" > 0 )); then
+	echo "identified audio" >> "$LOG"
+	abcde -d "$DEVNAME"
 
-elif [ $ID_FS_TYPE == "iso9660" ]; then
-	echo "identified data" >> $LOG
-	/opt/arm/data_rip.sh $LOG
-	eject $DEVNAME
+elif [ "$ID_FS_TYPE" == "iso9660" ]; then
+	echo "identified data" >> "$LOG"
+	/opt/arm/data_rip.sh "$LOG"
+	eject "$DEVNAME"
 else
-	echo "unable to identify" >> $LOG
-	echo $ID_CDROM_MEDIA_TRACK_COUNT_AUDIO >> $LOG
-	echo $ID_FS_TYPE >> $LOG
-	eject $DEVNAME
+	echo "unable to identify"
+	echo "$ID_CDROM_MEDIA_TRACK_COUNT_AUDIO" >> "$LOG"
+	echo "$ID_FS_TYPE" >> "$LOG"
+	eject "$DEVNAME"
 fi
 
 
-} >> $LOG
+} >> "identify-$LOG"
