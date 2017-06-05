c0b5cdfaf665b60c490b5fab67eb770c8d75bda7
Chris A. Bunt <cbunt1@yahoo.com>
2017-06-04 21:39:41 -0500

    removed legacy code
    removed deprecated timesamp code

--- identify.sh
+++ identify.sh
@@ -17,7 +17,6 @@ mkdir -p "$LOGPATH"
 # excludes sensative parameters
 # shellcheck disable=SC2129
 echo "*** Start config parameters ****" >> "$LOG"
-echo "Timestamp: [`date`]" >> "$LOG"
 echo -e "\tTimestamp: $(date -R)" >> "$LOG"
 # shellcheck disable=SC2002
 cat "$ARM_CONFIG"|sed '/^[#;].*$/d;/^$/d;/if/d;/^ /d;/^else/d;/^fi/d;/KEY=/d;/PASSWORD/d' >> "$LOG"
