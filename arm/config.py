2fc765efd6368a98d83b394e9541b7af4f36c272
muckngrind4 <derek@ourlittlezoo.com>
2017-08-12 18:19:08 -0700

    Limit = sub to 1 instance

--- arm/config.py
+++ arm/config.py
@@ -17,7 +17,7 @@ with open("/opt/arm/config", 'r') as f:
                 # print(line.strip())
                 line = re.sub("true", "\"true\"", line)
                 line = re.sub("false", "\"false\"", line)
-                line = re.sub("=", ": ", line)
+                line = re.sub("=", ": ", line,1)
                 of.writelines(line)
 
 with open("/opt/arm/arm/config.yaml", "r") as f:
