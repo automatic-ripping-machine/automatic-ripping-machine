257d5f4c51b52ae5c5c434a6819d506805842cc2
Derek <derek@ourlittlezoo.com>
2017-08-20 22:02:53 -0700

    Added IFTTT notifications

--- arm/notify.py
+++ arm/notify.py
@@ -2,21 +2,23 @@
 
 import os
 import logger
+import requests
 import argparse
 
 
 from pushbullet import Pushbullet
+import pyfttt as pyfttt
 from config import cfg
 
 # def entry():
 #     """ Entry to program, parses arguments
 #     This is just for testing..."""
 
-#     parser = argparse.ArgumentParser(description='Send notifications')
-#     parser.add_argument('-t', '--title', help='Title', required=True)
-#     parser.add_argument('-b', '--body', help='Body', required=True)
+    # parser = argparse.ArgumentParser(description='Send notifications')
+    # parser.add_argument('-t', '--title', help='Title', required=True)
+    # parser.add_argument('-b', '--body', help='Body', required=True)
 
-#     return parser.parse_args()
+    # return parser.parse_args()
 
 def notify(title, body):
     # Send notificaions
@@ -28,6 +30,12 @@ def notify(title, body):
         push = pb.push_note(title, body)
         # for key, value in push.items():
         #     print (key, value)
+
+    if cfg['IFTTT_KEY'] is not None:
+        
+        event = cfg['IFTTT_EVENT']
+        pyfttt.send_event(cfg['IFTTT_KEY'], event, title, body)
+
 
 # args = entry()
 
