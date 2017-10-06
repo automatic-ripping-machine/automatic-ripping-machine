45ecb6799d73d5612652a520fa1820190839ae6f
audioeng <audioeng@gmail.com>
2017-10-06 00:20:55 -0400

    Added label to notification

--- identify.sh
+++ identify.sh
@@ -117,7 +117,7 @@ elif [ -n "$ID_CDROM_MEDIA_TRACK_COUNT_AUDIO" ]; then
 	echo "identified audio" >> "$LOG"
 	abcde -d "$DEVNAME"
     if [ "$NOTIFY_RIP" = "true" ]; then
-	    echo /opt/arm/notify.sh "\"Audio Rip completed from ${DEVNAME}\"" |at now
+	    echo /opt/arm/notify.sh "\"Audio Rip: ${ID_FS_LABEL} completed from ${DEVNAME}\"" |at now
 	fi
 
 elif [ "$ID_FS_TYPE" == "iso9660" ]; then
