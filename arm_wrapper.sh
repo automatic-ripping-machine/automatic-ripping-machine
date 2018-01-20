a8671aeba3982d20ecd2e2104f4e4089136d387d
Derek <derek@ourlittlezoo.com>
2018-01-19 20:04:01 -0800

    More project structure

--- arm_wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# DEVNAME=$1
-# udevadm info --query=env "/dev/$DEVNAME" > /tmp/arm_disc_info_"$DEVNAME"
-# echo bash /opt/arm/identify.sh /opt/arm/config /tmp/arm_disc_info_"$DEVNAME" | at now
-# echo bash /opt/arm/identify.sh /opt/arm/config | at now
-# echo "This is $DEVNAME" >> /opt/arm/log/empty.log | as now
-# echo /usr/bin/python3 /opt/arm/arm/main.py -l "BOSS" -d "/dev/sr0" | at now
