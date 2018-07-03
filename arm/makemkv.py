46e3c05f392d55a4d92294e698725f7550add04a
budgidiere <sgidiere@gmail.com>
2018-07-03 09:15:40 -0500

    Update makemkv.py

--- arm/makemkv.py
+++ arm/makemkv.py
@@ -24,7 +24,7 @@ def makemkv(logfile, disc):
 
     Returns path to ripped files.
     """
-    if autograbcode == True:
+    if autograbcode is True:
         logging.info("Auto Grabbing MakeMKV Code")
         grabcode()
     logging.info("Starting MakeMKV rip. Method is " + cfg['RIPMETHOD'])
