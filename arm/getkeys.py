fe903e6ebba3d671f483278b1a8401367a98913a
budgidiere <sgidiere@gmail.com>
2018-07-02 20:06:27 -0500

    Update getkeys.py

--- arm/getkeys.py
+++ arm/getkeys.py
@@ -7,20 +7,20 @@ def grabkeys():
     br.open('http://makemkv.com/forum2/viewtopic.php?f=12&t=16959')
     pageStr = str(br.parsed())
     i = 1
-    
+
     def get_key_link(base_link):
         global i, pageStr
         beg = pageStr.find(base_link)
         strLength = len(base_link)
-        
+
         while True:
             link = pageStr[beg:beg + strLength + i]
             print(link)
-            
-            if pageStr[beg + strLength:beg + strLength + i].isnumeric() == False:
+
+            if pageStr[beg + strLength:beg + strLength + i].isnumeric() is False:
                 return link[:-1]
                 i = i + 1
-                
+
         # print(get_key_link())
         os.system('tinydownload -o keys_hashed.txt ' + get_key_link('http://s000.tinyupload.com/index.php?file_id='))
         br.open('https://forum.doom9.org/showthread.php?t=175194')
