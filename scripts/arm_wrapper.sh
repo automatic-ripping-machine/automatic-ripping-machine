04b55c456409c5b701fcdda03934ede076a13970
flammableliquids <geforbes@gmail.com>
2020-07-11 13:36:07 +1000

    Update to arm_wrapper.sh
    This modification starts the Webui if it isn't already started.
    Fixing a frequently occurring annoyance.

--- scripts/arm_wrapper.sh
+++ scripts/arm_wrapper.sh
@@ -2,5 +2,16 @@
 
 DEVNAME=$1
 
+# Check to see if the admin page is running, if not, start it
+if pgrep -f "runui.py" > /dev/null
+then
+        echo "ARM Webgui running  on " | logger -t ARM
+else
+        echo "ARM Webgui not running  on starting it "| logger -t ARM
+        /bin/su -l -c "/usr/bin/python3 /opt/arm/arm/runui.py  " -s /bin/bash arm
+        # Try the below line if you want it to log to your log file of choice
+        #/bin/su -l -c "/usr/bin/python3 /opt/arm/arm/runui.py  &> [pathtologDir]/WebUI.log" -s /bin/bash arm
+fi
+
 echo "Starting ARM on ${DEVNAME}" | logger -t ARM
 /bin/su -l -c "echo /usr/bin/python3 /opt/arm/arm/main.py -d ${DEVNAME} | at now" -s /bin/bash arm
