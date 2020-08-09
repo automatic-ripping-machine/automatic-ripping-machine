f822e1b28272ccf5c7cb09c6c9248d38c4569501
flammableliquids <geforbes@gmail.com>
2020-08-09 14:43:58 +1000

    Updated forms.py
    Also added the custom title form. Custom Title form is intended for use for DVD's that have multiple disk (al ah Lord of the rings), Or TV Shows where you want to rip 1 disk after another or inparalle.

--- arm/ui/forms.py
+++ arm/ui/forms.py
@@ -8,11 +8,16 @@ class TitleSearchForm(FlaskForm):
     year = StringField('Year')
     submit = SubmitField('Submit')
 
+class CustomTitleForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    year = StringField('Year')
+    submit = SubmitField('Submit')
+
 
 class ChangeParamsForm(FlaskForm):
     RIPMETHOD = SelectField('Rip Method', choices=[('mkv', 'mkv'), ('backup', 'backup')])
-    MAINFEATURE = BooleanField('Main Feature', validators=[DataRequired()])
     #MAINFEATURE = BooleanField('Main Feature', validators=[DataRequired()])
+    MAINFEATURE = BooleanField('Main Feature')
     MINLENGTH = IntegerField('Minimum Length')
     MAXLENGTH = IntegerField('Maximum Length')
     submit = SubmitField('Submit')
