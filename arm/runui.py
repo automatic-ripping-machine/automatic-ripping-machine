f63b559f78057baab9d127f6ec5b42a08e61dcad
Joe Skeen <joeskeen@outlook.com>
2020-08-24 12:50:15 -0600

    autodetect IP address if not supplied in config

--- arm/runui.py
+++ arm/runui.py
@@ -5,6 +5,22 @@ from arm.ui import app  # noqa E402
 from arm.config.config import cfg  # noqa E402
 import arm.ui.routes  # noqa E402
 
+host = cfg['WEBSERVER_IP']
+if host == 'x.x.x.x':
+    # autodetect host IP address
+    from netifaces import interfaces, ifaddresses, AF_INET
+    ip_list = []
+    for interface in interfaces():
+        inet_links = ifaddresses(interface).get(AF_INET, [])
+        for link in inet_links:
+            ip = link['addr']
+            if ip != '127.0.0.1':
+                ip_list.append(ip)
+    if len(ip_list) > 0:
+        host = ip_list[0]
+    else:
+        host = '127.0.0.1'
+
 if __name__ == '__main__':
-    app.run(host=cfg['WEBSERVER_IP'], port=cfg['WEBSERVER_PORT'], debug=True)
+    app.run(host=host, port=cfg['WEBSERVER_PORT'], debug=True)
     # app.run(debug=True)
