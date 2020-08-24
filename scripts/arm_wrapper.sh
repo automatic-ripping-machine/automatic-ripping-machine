aaed55f7c3b2d071f59493de6d48b6048ff14725
Joe Skeen <joeskeen@outlook.com>
2020-08-23 22:09:02 -0600

    improve logging for system logs and console logs

--- scripts/arm_wrapper.sh
+++ scripts/arm_wrapper.sh
@@ -2,15 +2,15 @@
 
 DEVNAME=$1
 
-echo "Starting ARM on ${DEVNAME}" | logger -t ARM
+echo "[ARM] Starting ARM on ${DEVNAME}" | logger -t ARM -s
 /bin/su -l -c "echo /usr/bin/python3 /opt/arm/arm/ripper/main.py -d ${DEVNAME} | at now" -s /bin/bash arm
 
 # Check to see if the admin page is running, if not, start it
 if pgrep -f "runui.py" > /dev/null
 then
-        echo "ARM Webgui running  on " | logger -t ARM
+        echo "[ARM] ARM Webgui running. " | logger -t ARM -s
 else
-        echo "ARM Webgui not running  on starting it "| logger -t ARM
+        echo "[ARM] ARM Webgui not running; starting it "| logger -t ARM -s
         /bin/su -l -c "/usr/bin/python3 /opt/arm/arm/runui.py  " -s /bin/bash arm
         # Try the below line if you want it to log to your log file of choice
         #/bin/su -l -c "/usr/bin/python3 /opt/arm/arm/runui.py  &> [pathtologDir]/WebUI.log" -s /bin/bash arm
