bc674fab45225cd12b3879a051f48e8854000a6d
1337-server <62730074+1337-server@users.noreply.github.com>
2020-11-13 13:48:10 +0000

    Update debian-setup.sh

--- scripts/debian-setup.sh
+++ scripts/debian-setup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-RED='\033[0;31m'
+RED='\033[1;31m'
 NC='\033[0m' # No Color
 
-echo -e "${RED} Adding arm user${NC}"
+echo -e "${RED}Adding arm user${NC}"
 groupadd arm
 useradd -m arm -g arm -G cdrom
 passwd arm
 echo -e "${RED}Installing git${NC}"
 apt-get install git
-echo "${RED}installing required build tools${NC}"
+echo -e "${RED}Installing required build tools${NC}"
 apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev
-echo "${RED}installing wget${NC}"
+echo -e "${RED}Installing wget${NC}"
 apt-get install wget
 
-echo "${RED}setting up directories and getting makeMKV files${NC}"
+echo -e "${RED}Setting up directories and getting makeMKV files${NC}"
 mkdir /makeMKV
 cd /makeMKV
 
 wget https://www.makemkv.com/download/makemkv-bin-1.15.3.tar.gz
 wget https://www.makemkv.com/download/makemkv-oss-1.15.3.tar.gz
 
-echo "${RED}Extracting MakeMKV${NC}"
+echo -e "${RED}Extracting MakeMKV${NC}"
 tar xvzf makemkv-oss-1.15.3.tar.gz
 tar xvzf makemkv-bin-1.15.3.tar.gz
 
 cd makemkv-oss-1.15.3
-echo "${RED}Installing MakeMKV${NC}"
+echo -e "${RED}Installing MakeMKV${NC}"
 ./configure
 make
 make install
@@ -34,20 +34,17 @@ cd ../makemkv-bin-1.15.3
 make
 make install
 
-#cd ../..
-#mkdir ffmpeg
-#cd ffmpeg
-echo "${RED}Installing ffmpeg${NC}"
-apt-get install ffmpeg
+echo -e "${RED}Installing ffmpeg${NC}"
+apt install ffmpeg
 
-echo "${RED}Installing ARM requirments{NC}"
+echo -e "${RED}Installing ARM requirments${NC}"
 apt install handbrake-cli libavcodec-extra
 apt install libdvdcss2
 apt install abcde flac imagemagick glyrc cdparanoia
 apt install at
 apt install python3 python3-pip
-apt-get install libcurl4-openssl-dev libssl-dev  
-apt-get install libdvd-pkg
+apt install libcurl4-openssl-dev libssl-dev  
+apt install libdvd-pkg
 wget http://download.videolan.org/pub/debian/stable/libdvdcss2_1.2.13-0_amd64.deb
 wget http://download.videolan.org/pub/debian/stable/libdvdcss_1.2.13-0.debian.tar.gz
 wget http://ftp.us.debian.org/debian/pool/contrib/libd/libdvd-pkg/libdvd-pkg_1.4.0-1-2_all.deb
@@ -58,7 +55,7 @@ dpkg-reconfigure libdvd-pkg
 apt install default-jre-headless
 apt install eject
 
-echo "${RED}Installing ARM:Automatic Ripping Machine${NC}"
+echo -e "${RED}Installing ARM:Automatic Ripping Machine${NC}"
 cd /opt
 mkdir arm
 chown arm:arm arm
@@ -72,7 +69,7 @@ cd arm
 # TODO: Remove below line before merging to master
 git checkout v2.1_dev
 pip3 install setuptools
-sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+apt-get install python3-dev python3-pip python3-venv python3-wheel -y
 pip3 install wheel
 pip3 install -r requirements.txt 
 ln -s /opt/arm/setup/51-automedia.rules /lib/udev/rules.d/
@@ -83,16 +80,12 @@ ln -s /opt/arm/arm.yaml /etc/arm/
 
 mkdir -p /mnt/dev/sr0
 
-#nano /etc/fstab
-#/dev/sr0  /mnt/dev/sr0  udf,iso9660  user,noauto,exec,utf8  0  0 
-########
+######## adding new line to fstab, needed for the autoplay to work
 echo -e "${RED}Adding fstab entry${NC}"
-echo -e "/dev/sr0  /mnt/dev/sr0  udf,iso9660  user,noauto,exec,utf8  0  0 \n" >> /etc/fstab
+echo -e "\n/dev/sr0  /mnt/dev/sr0  udf,iso9660  user,noauto,exec,utf8  0  0 \n" >> /etc/fstab
 
-#####run the ui , set as cron or service
-###add as service
-# python3 /opt/arm/arm/runui.py
-echo "${RED}Installing ARM service${NC}"
+#####run the ARM ui as a service
+echo -e "${RED}Installing ARM service${NC}"
 cat > /etc/systemd/system/armui.service <<- EOM
 [Unit]
 Description=Arm service
@@ -108,4 +101,4 @@ ExecStart=python3 /opt/arm/arm/runui.py
 WantedBy=multi-user.target
 EOM
 #echo -e "" >> /etc/systemd/system/armui.service
-echo systemctl start armui
+systemctl start armui
