ae8130491a283c3c4128cf4d24e0de54fc7eccb2
1337-server <62730074+1337-server@users.noreply.github.com>
2020-11-14 05:06:02 +0000

    Update arm_wrapper.sh

--- scripts/arm_wrapper.sh
+++ scripts/arm_wrapper.sh
@@ -3,4 +3,6 @@
 DEVNAME=$1
 
 echo "Starting ARM on ${DEVNAME}" | logger -t ARM
-/bin/su -l -c "echo /usr/bin/python3 /opt/arm/arm/ripper/main.py -d ${DEVNAME} | at now" -s /bin/bash arm
+#/bin/su -l -c "echo /usr/bin/python3 /opt/arm/arm/ripper/main.py -d ${DEVNAME} | at now" -s /bin/bash arm
+#wont run unless run as root user
+/bin/su -l -c "echo /usr/bin/python3 /opt/arm/arm/ripper/main.py -d ${DEVNAME} | at now" -s /bin/bash root
