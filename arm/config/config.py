7459b031890a2416ae70cd17c8fb1e49fbe6a6ed
1337-server <62730074+1337-server@users.noreply.github.com>
2020-12-04 09:03:31 +0000

    Removes security warning for yaml.load
    More info available here: https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation

--- arm/config/config.py
+++ arm/config/config.py
@@ -7,4 +7,4 @@ import yaml
 yamlfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../..", "arm.yaml")
 
 with open(yamlfile, "r") as f:
-    cfg = yaml.load(f)
+    cfg = yaml.load(f,Loader=yaml.FullLoader)
