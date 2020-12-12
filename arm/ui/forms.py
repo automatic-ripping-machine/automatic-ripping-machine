dacaf05a40ce52a6f0fba570f2832b7fe0719595
1337-server <62730074+1337-server@users.noreply.github.com>
2020-12-12 06:03:00 +0000

    Update to fix the mainfeature bug

--- arm/ui/forms.py
+++ arm/ui/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, IntegerField, BooleanField
+from wtforms import StringField, SubmitField, SelectField, IntegerField, BooleanField, PasswordField, validators
 from wtforms.validators import DataRequired
 
 
@@ -15,9 +15,16 @@ class CustomTitleForm(FlaskForm):
 
 
 class ChangeParamsForm(FlaskForm):
-    RIPMETHOD = SelectField('Rip Method', choices=[('mkv', 'mkv'), ('backup', 'backup')])
-    #MAINFEATURE = BooleanField('Main Feature', validators=[DataRequired()])
-    MAINFEATURE = BooleanField('Main Feature')
-    MINLENGTH = IntegerField('Minimum Length')
-    MAXLENGTH = IntegerField('Maximum Length')
+    RIPMETHOD = SelectField('Rip Method: ', choices=[('mkv', 'mkv'), ('backup', 'backup')])
+    # "music", "dvd", "bluray" and "data"
+    DISCTYPE = SelectField('Disc Type: ', choices=[('dvd', 'DVD'), ('bluray', 'Bluray'), ('music', 'Music'), ('data', 'Data')])
+    MAINFEATURE = BooleanField('Main Feature: ')
+    #MAINFEATURE = SelectField('Main Feature: ', choices=[(1, 'Yes'), (0, 'No')])
+    MINLENGTH = IntegerField('Minimum Length: ')
+    MAXLENGTH = IntegerField('Maximum Length: ')
+    submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    password = PasswordField('Password')
     submit = SubmitField('Submit')
