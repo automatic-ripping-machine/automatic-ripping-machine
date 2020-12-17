124d293279097165627f6c3990b3bca1a53ada45
1337-server <62730074+1337-server@users.noreply.github.com>
2020-12-17 06:58:34 +0000

    Change to use the utils.cleanlog function

--- arm/ripper/identify.py
+++ arm/ripper/identify.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-
 # Identification of dvd/bluray
 
 import os
-import sys # noqa # pylint: disable=unused-import
+import sys  # noqa # pylint: disable=unused-import
 import logging
 import urllib
 import re
@@ -13,10 +12,12 @@ import unicodedata
 import xmltodict
 import json
 
-#import getmusictitle
+# import getmusictitle
 from arm.ripper import getmusictitle
 from arm.ripper import utils
 from arm.ui import db
+
+
 # from arm.config.config import cfg
 
 # flake8: noqa: W605
@@ -24,9 +25,10 @@ from arm.ui import db
 
 def identify(job, logfile):
     """Identify disc attributes"""
-    ## Safe way of dealing with log files if the users need to post it online
-    #cleanlog = makecleanlogfile(job)
-    #logging.debug("Identify Entry point --- job ----"+ str(cleanlog))
+
+    # Safe way of dealing with log files if the users need to post it online
+    # cleanlog = utils.makecleanlogfile(job)
+    # logging.debug("Identify Entry point --- job ----"+ str(cleanlog))
 
     logging.info("Mounting disc to: " + str(job.mountpoint))
 
@@ -69,17 +71,17 @@ def identify(job, logfile):
             if job.disctype == "bluray":
                 res = identify_bluray(job)
             ## Need to check if year is "0000"  or ""
-            if res and not job.year == "0000":
-                ## only get video details if its a bluray
+            if res and job.year != "0000":
                 get_video_details(job)
             else:
                 job.hasnicetitle = False
                 db.session.commit()
 
-            logging.info("Disc title Post ident: " + str(job.title) + " : " + str(job.year) + " : " + str(job.video_type))
+            logging.info(
+                "Disc title Post ident: " + str(job.title) + " : " + str(job.year) + " : " + str(job.video_type))
             ## Safe way of dealing with log files if the users need to post it online
-            cleanlog = makecleanlogfile(job)
-            logging.debug("identify.job.end ----" + str(cleanlog))
+            # cleanlog = utils.makecleanlogfile(job)
+            # logging.debug("identify.job.end ----" + str(cleanlog))
 
     os.system("umount " + job.devpath)
 
@@ -98,7 +100,8 @@ def clean_for_filename(string):
     ## Added from pull 366
     # testing why the return function isn't cleaning
     return re.sub('[^\w\-_\.\(\) ]', '', string)
-    #return string
+    # return string
+
 
 def identify_bluray(job):
     """ Get's Blu-Ray title by parsing XML in bdmt_eng.xml """
@@ -107,8 +110,8 @@ def identify_bluray(job):
         with open(job.mountpoint + '/BDMV/META/DL/bdmt_eng.xml', "rb") as xml_file:
             doc = xmltodict.parse(xml_file.read())
     except OSError as e:
-        logging.error("Disc is a bluray, but bdmt_eng.xml could not be found.  Disc cannot be identified.  Error number is: " + str(e.errno))
-        # Fix for blurays with no label causing crashes
+        logging.error("Disc is a bluray, but bdmt_eng.xml could not be found.  Disc cannot be identified.  Error "
+                      "number is: " + str(e.errno))
         job.title = "not identified"
         job.year = ""
         db.session.commit()
@@ -118,9 +121,7 @@ def identify_bluray(job):
         bluray_title = doc['disclib']['di:discinfo']['di:title']['di:name']
     except KeyError:
         ## Changed from pull 366
-        # Fallback title isnt declared ?
-        #bluray_title = str(fallback_title)
-        bluray_ttitle = "not identified"
+        bluray_title = "not identified"
         bluray_year = ""
         logging.error("Could not parse title from bdmt_eng.xml file.  Disc cannot be identified.")
         # return False
@@ -146,15 +147,12 @@ def identify_bluray(job):
 
 
 def identify_dvd(job):
-    """ Calculates CRC64 for the DVD and calls Windows Media
-        Metaservices and returns the Title and year of DVD """
-    ## Added from pull 366
     """ Manipulates the DVD title and calls OMDB to try and 	
     lookup the title """
 
     ## Safe way of dealing with log files if the users need to post it online
-    #cleanlog = makecleanlogfile(job)
-    #logging.debug("####### --- job ----" + str(cleanlog))
+    # cleanlog = utils.makecleanlogfile(job)
+    # logging.debug("####### --- job ----" + str(cleanlog))
 
     ## Added from #338
     # Some older DVDs aren't actually labelled
@@ -169,17 +167,15 @@ def identify_dvd(job):
         crc64 = pydvdid.compute(str(job.mountpoint))
         fallback_title = "{0}_{1}".format(str(job.label), str(crc64))
         dvd_title = str(fallback_title)
+        logging.info("DVD CRC64 hash is: " + str(crc64))
+        job.crc_id = str(crc64)
+        ## Dead needs removing
+        urlstring = "http://metaservices.windowsmedia.com/pas_dvd_B/template/GetMDRDVDByCRC.xml?CRC={0}".format(
+            str(crc64))
+        logging.debug(urlstring)
     except pydvdid.exceptions.PydvdidException as e:
         logging.error("Pydvdid failed with the error: " + str(e))
         dvd_title = fallback_title = str(job.label)
-
-
-    logging.info("DVD CRC64 hash is: " + str(crc64))
-    job.crc_id = str(crc64)
-
-    ## Dead needs removing
-    urlstring = "http://metaservices.windowsmedia.com/pas_dvd_B/template/GetMDRDVDByCRC.xml?CRC={0}".format(str(crc64))
-    logging.debug(urlstring)
 
     dvd_title = job.label
     logging.debug("dvd_title_label= " + str(dvd_title))
@@ -196,7 +192,7 @@ def identify_dvd(job):
 
     # try to contact omdb
     try:
-        dvd_info_xml = callwebservice(job, job.config.OMDB_API_KEY , dvd_title, year)
+        dvd_info_xml = callwebservice(job, job.config.OMDB_API_KEY, dvd_title, year)
         logging.debug("DVD_INFO_XML: " + str(dvd_info_xml))
     except OSError as e:
         # we couldnt reach omdb
@@ -207,6 +203,7 @@ def identify_dvd(job):
     job.title = dvd_title
     db.session.commit()
     return True
+
 
 def get_video_details(job):
     """ Clean up title and year.  Get video_type, imdb_id, poster_url from
@@ -243,7 +240,7 @@ def get_video_details(job):
     omdb_api_key = job.config.OMDB_API_KEY
 
     logging.debug("Title: " + title + " | Year: " + year)
-    logging.debug("Calling webservice with title: " + title  + " and year: " + year)
+    logging.debug("Calling webservice with title: " + title + " and year: " + year)
 
     ## Callwebservice already handles commiting to database, no need for identify_dvd()
     response = callwebservice(job, omdb_api_key, title, year)
@@ -298,11 +295,18 @@ def callwebservice(job, omdb_api_key, dvd_title, year=""):
     """ Queries OMDbapi.org for title information and parses type, imdb, and poster info
     """
     if job.config.VIDEOTYPE == "auto":
-        strurl = "http://www.omdbapi.com/?t={1}&y={2}&plot=short&r=json&apikey={0}".format(omdb_api_key, dvd_title, year)
-        logging.debug("http://www.omdbapi.com/?t={1}&y={2}&plot=short&r=json&apikey={0}".format("key_hidden", dvd_title, year))
+        strurl = "http://www.omdbapi.com/?t={1}&y={2}&plot=short&r=json&apikey={0}".format(omdb_api_key, dvd_title,
+                                                                                           year)
+        logging.debug(
+            "http://www.omdbapi.com/?t={1}&y={2}&plot=short&r=json&apikey={0}".format("key_hidden", dvd_title, year))
     else:
-        strurl = "http://www.omdbapi.com/?t={1}&y={2}&type={3}&plot=short&r=json&apikey={0}".format(omdb_api_key, dvd_title, year, job.config.VIDEOTYPE)
-        logging.debug("http://www.omdbapi.com/?t={1}&y={2}&type={3}&plot=short&r=json&apikey={0}".format("key_hidden", dvd_title, year, job.config.VIDEOTYPE))
+        strurl = "http://www.omdbapi.com/?t={1}&y={2}&type={3}&plot=short&r=json&apikey={0}".format(omdb_api_key,
+                                                                                                    dvd_title, year,
+                                                                                                    job.config.VIDEOTYPE)
+        logging.debug(
+            "http://www.omdbapi.com/?t={1}&y={2}&type={3}&plot=short&r=json&apikey={0}".format("key_hidden", dvd_title,
+                                                                                               year,
+                                                                                               job.config.VIDEOTYPE))
 
     logging.debug("***Calling webservice with Title: " + str(dvd_title) + " and Year: " + str(year))
     try:
@@ -336,105 +340,3 @@ def callwebservice(job, omdb_api_key, dvd_title, year=""):
             job.hasnicetitle = True
             db.session.commit()
             return doc['Response']
-
-## (PB_KEY=**REMOVED**)  || \(PB_KEY=.*?\)
-## (EMBY_PASSWORD=) || \(EMBY_PASSWORD=.*?\)
-## (EMBY_API_KEY=) || \(EMBY_API_KEY=.*?\)
-## (EMBY_SERVER=) || \(EMBY_SERVER=.*?\)
-## (IFTTT_KEY=) || \(IFTTT_KEY=.*?\)
-## (OMDB_API_KEY=**REMOVED**) || \(OMDB_API_KEY=.*?\)
-## (PO_APP_KEY=)  || \(PO_APP_KEY=.*?\)
-## (PO_USER_KEY=) || \(PO_USER_KEY=.*?\)
-## function to clean the debug log of secret keys
-def makecleanlogfile(logfile):
-    ##TODO: make this cleaner/smaller
-
-    ##lets make sure we are using a string
-    logfile=str(logfile)
-    ## TODO: maybe check if the ip is local, if its not strip it from log or add some part protection eg: 89.89.xx.xx
-    ## WEBSERVER_IP: x.x.x.x
-    ## logging.debug("inside makecleanlogfile: " + str(logfile) + "\n\r")
-    out = re.sub("\(PB_KEY=.*?\)", '(PB_KEY=** REMOVED **)', logfile)
-    out = re.sub("\(EMBY_PASSWORD=.*?\)", '(EMBY_PASSWORD=** REMOVED **)', out)
-    out = re.sub("\(EMBY_API_KEY=.*?\)", '(EMBY_API_KEY=** REMOVED **)', out)
-    out = re.sub("\(EMBY_SERVER=.*?\)", '(EMBY_SERVER=** REMOVED **)', out)
-    out = re.sub("\(IFTTT_KEY=.*?\)", '(IFTTT_KEY=** REMOVED **)', out)
-    out = re.sub("\(OMDB_API_KEY=.*?\)", '(OMDB_API_KEY=** REMOVED **)', out)
-    out = re.sub("\(PO_APP_KEY=.*?\)", '(PO_APP_KEY=** REMOVED **)', out)
-    out = re.sub("\(PO_USER_KEY=.*?\)", '(PO_USER_KEY=** REMOVED **)', out)
-
-    ## Apprise notifications
-    out = re.sub("\(DISCORD_WEBHOOK_ID=.*?\)", '(DISCORD_WEBHOOK_ID=** REMOVED **)', out)
-    out = re.sub("\(DISCORD_TOKEN=.*?\)", '(DISCORD_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(FAAST_TOKEN=.*?\)", '(FAAST_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(FLOCK_TOKEN=.*?\)", '(FLOCK_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(GITTER_TOKEN=.*?\)", '(GITTER_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(GOTIFY_HOST=.*?\)", '(GOTIFY_HOST=** REMOVED **)', out)
-    out = re.sub("\(GROWL_HOST=.*?\)", '(GROWL_HOST=** REMOVED **)', out)
-    out = re.sub("\(GROWL_PASS=.*?\)", '(GROWL_PASS=** REMOVED **)', out)
-    out = re.sub("\(JOIN_API=.*?\)", '(JOIN_API=** REMOVED **)', out)
-    out = re.sub("\(JOIN_DEVICE=.*?\)", '(JOIN_DEVICE=** REMOVED **)', out)
-    out = re.sub("\(KODI_HOST=.*?\)", '(KODI_HOST=** REMOVED **)', out)
-    out = re.sub("\(KODI_PASS=.*?\)", '(KODI_PASS=** REMOVED **)', out)
-    out = re.sub("\(KUMULOS_API=.*?\)", '(KUMULOS_API=** REMOVED **)', out)
-    out = re.sub("\(LAMETRIC_API=.*?\)", '(LAMETRIC_API=** REMOVED **)', out)
-    out = re.sub("\(LAMETRIC_HOST=.*?\)", '(LAMETRIC_HOST=** REMOVED **)', out)
-    out = re.sub("\(LAMETRIC_APP_ID=.*?\)", '(LAMETRIC_APP_ID=** REMOVED **)', out)
-    out = re.sub("\(LAMETRIC_TOKEN=.*?\)", '(LAMETRIC_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(MAILGUN_DOMAIN=.*?\)", '(MAILGUN_DOMAIN=** REMOVED **)', out)
-    out = re.sub("\(MAILGUN_APIKEY=.*?\)", '(MAILGUN_APIKEY=** REMOVED **)', out)
-    out = re.sub("\(MATRIX_HOST=.*?\)", '(MATRIX_HOST=** REMOVED **)', out)
-    out = re.sub("\(MATRIX_PASS=.*?\)", '(MATRIX_PASS=** REMOVED **)', out)
-    out = re.sub("\(MATRIX_TOKEN=.*?\)", '(MATRIX_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(MSTEAMS_TOKENA=.*?\)", '(MSTEAMS_TOKENA=** REMOVED **)', out)
-    out = re.sub("\(MSTEAMS_TOKENB=.*?\)", '(MSTEAMS_TOKENB=** REMOVED **)', out)
-    out = re.sub("\(MSTEAMS_TOKENC=.*?\)", '(MSTEAMS_TOKENC=** REMOVED **)', out)
-    out = re.sub("\(NEXTCLOUD_HOST=.*?\)", '(NEXTCLOUD_HOST=** REMOVED **)', out)
-    out = re.sub("\(NEXTCLOUD_ADMINPASS=.*?\)", '(NEXTCLOUD_ADMINPASS=** REMOVED **)', out)
-    out = re.sub("\(NOTICA_TOKEN=.*?\)", '(NOTICA_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(NOTIFICO_PROJECTID=.*?\)", '(NOTIFICO_PROJECTID=** REMOVED **)', out)
-    out = re.sub("\(NOTIFICO_MESSAGEHOOK=.*?\)", '(NOTIFICO_MESSAGEHOOK=** REMOVED **)', out)
-    out = re.sub("\(OFFICE365_TENANTID=.*?\)", '(OFFICE365_TENANTID=** REMOVED **)', out)
-    out = re.sub("\(OFFICE365_CLIENT_ID=.*?\)", '(OFFICE365_CLIENT_ID=** REMOVED **)', out)
-    out = re.sub("\(OFFICE365_CLIENT_SECRET=.*?\)", '(OFFICE365_CLIENT_SECRET=** REMOVED **)', out)
-    out = re.sub("\(POPCORN_API=.*?\)", '(POPCORN_API=** REMOVED **)', out)
-    out = re.sub("\(POPCORN_EMAIL=.*?\)", '(POPCORN_EMAIL=** REMOVED **)', out)
-    out = re.sub("\(POPCORN_PHONENO=.*?\)", '(POPCORN_PHONENO=** REMOVED **)', out)
-    out = re.sub("\(PROWL_API=.*?\)", '(PROWL_API=** REMOVED **)', out)
-    out = re.sub("\(PROWL_PROVIDERKEY=.*?\)", '(PROWL_PROVIDERKEY=** REMOVED **)', out)
-    out = re.sub("\(PUSH_API=.*?\)", '(PUSH_API=** REMOVED **)', out)
-    out = re.sub("\(PUSHED_APP_KEY=.*?\)", '(PUSHED_APP_KEY=** REMOVED **)', out)
-    out = re.sub("\(PUSHED_APP_SECRET=.*?\)", '(PUSHED_APP_SECRET=** REMOVED **)', out)
-    out = re.sub("\(PUSHSAFER_KEY=.*?\)", '(PUSHSAFER_KEY=** REMOVED **)', out)
-    out = re.sub("\(ROCKETCHAT_HOST=.*?\)", '(ROCKETCHAT_HOST=** REMOVED **)', out)
-    out = re.sub("\(ROCKETCHAT_PASS=.*?\)", '(ROCKETCHAT_PASS=** REMOVED **)', out)
-    out = re.sub("\(ROCKETCHAT_WEBHOOK=.*?\)", '(ROCKETCHAT_WEBHOOK=** REMOVED **)', out)
-    out = re.sub("\(RYVER_ORG=.*?\)", '(RYVER_ORG=** REMOVED **)', out)
-    out = re.sub("\(RYVER_TOKEN=.*?\)", '(RYVER_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(SENDGRID_API=.*?\)", '(SENDGRID_API=** REMOVED **)', out)
-    out = re.sub("\(SENDGRID_FROMMAIL=.*?\)", '(SENDGRID_FROMMAIL=** REMOVED **)', out)
-    out = re.sub("\(SIMPLEPUSH_API=.*?\)", '(SIMPLEPUSH_API=** REMOVED **)', out)
-    out = re.sub("\(SLACK_TOKENA=.*?\)", '(SLACK_TOKENA=** REMOVED **)', out)
-    out = re.sub("\(SLACK_TOKENB=.*?\)", '(SLACK_TOKENB=** REMOVED **)', out)
-    out = re.sub("\(SLACK_TOKENC=.*?\)", '(SLACK_TOKENC=** REMOVED **)', out)
-    out = re.sub("\(SPARKPOST_API=.*?\)", '(SPARKPOST_API=** REMOVED **)', out)
-    out = re.sub("\(SPARKPOST_HOST=.*?\)", '(SPARKPOST_HOST=** REMOVED **)', out)
-    out = re.sub("\(SPONTIT_API=.*?\)", '(SPONTIT_API=** REMOVED **)', out)
-    out = re.sub("\(SPONTIT_USER_ID=.*?\)", '(SPONTIT_USER_ID=** REMOVED **)', out)
-    out = re.sub("\(TELEGRAM_BOT_TOKEN=.*?\)", '(TELEGRAM_BOT_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(TELEGRAM_CHAT_ID=.*?\)", '(TELEGRAM_CHAT_ID=** REMOVED **)', out)
-    out = re.sub("\(TWIST_EMAIL=.*?\)", '(TWIST_EMAIL=** REMOVED **)', out)
-    out = re.sub("\(TWIST_PASS=.*?\)", '(TWIST_PASS=** REMOVED **)', out)
-    out = re.sub("\(XBMC_HOST=.*?\)", '(XBMC_HOST=** REMOVED **)', out)
-    out = re.sub("\(XBMC_PASS=.*?\)", '(XBMC_PASS=** REMOVED **)', out)
-    out = re.sub("\(XMPP_HOST=.*?\)", '(XMPP_HOST=** REMOVED **)', out)
-    out = re.sub("\(XMPP_PASS=.*?\)", '(XMPP_PASS=** REMOVED **)', out)
-    out = re.sub("\(WEBEX_TEAMS_TOKEN=.*?\)", '(WEBEX_TEAMS_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(ZILUP_CHAT_TOKEN=.*?\)", '(ZILUP_CHAT_TOKEN=** REMOVED **)', out)
-    out = re.sub("\(ZILUP_CHAT_ORG=.*?\)", '(ZILUP_CHAT_ORG=** REMOVED **)', out)
-    ## format for more entries
-    ## out = re.sub("\(CONFIG_ID=.*?\)", '(CONFIG_ID=** REMOVED **)', out)
-
-
-    ##logging.debug("our clean log string" + str(out))
-    return out
