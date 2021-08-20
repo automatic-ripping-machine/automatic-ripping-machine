42d6d7d4ae0d54dc7947e41924467c96d74a60b5
mneimeyer <matt@neimeyer.org>
2021-08-19 21:44:01 -0400

    re-louden debian install script

--- scripts/debian-setup.sh
+++ scripts/debian-setup.sh
@@ -12,15 +12,20 @@ NC='\033[0m' # No Color
 echo -e "${RED}Adding arm user${NC}"
 groupadd arm
 useradd -m arm -g arm -G cdrom
-passwd arm
+# If a password was specified use that, otherwise prompt
+if [ -n "$1" ]; then
+    echo "$1" | passwd --stdin arm
+else
+    passwd arm
+fi
 
 echo -e "${RED}Installing git${NC}"
 apt update
-apt install -y git
+apt install git
 echo -e "${RED}Installing required build tools${NC}"
-apt install -y build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev
+apt install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev
 echo -e "${RED}Installing wget${NC}"
-apt install -y wget
+apt install wget
 
 echo -e "${RED}Setting up directories and getting makeMKV files${NC}"
 mkdir -p /makeMKV
@@ -55,17 +60,17 @@ make
 make install
 
 echo -e "${RED}Installing ffmpeg${NC}"
-apt install -y ffmpeg
+apt install ffmpeg
 
 echo -e "${RED}Installing ARM requirments${NC}"
-apt install -y handbrake-cli libavcodec-extra
-apt install -y libdvdcss2
-apt install -y abcde flac imagemagick glyrc cdparanoia
-apt install -y at
-apt install -y python3 python3-pip
-apt install -y libcurl4-openssl-dev libssl-dev
-apt install -y libdvd-pkg
-apt install -y lsdvd
+apt install handbrake-cli libavcodec-extra
+apt install libdvdcss2
+apt install abcde flac imagemagick glyrc cdparanoia
+apt install at
+apt install python3 python3-pip
+apt install libcurl4-openssl-dev libssl-dev
+apt install libdvd-pkg
+apt install  lsdvd
 wget http://download.videolan.org/pub/debian/stable/libdvdcss2_1.2.13-0_amd64.deb
 wget http://download.videolan.org/pub/debian/stable/libdvdcss_1.2.13-0.debian.tar.gz
 wget http://ftp.us.debian.org/debian/pool/contrib/libd/libdvd-pkg/libdvd-pkg_1.4.0-1-2_all.deb
@@ -73,8 +78,8 @@ sudo dpkg -i libdvdcss2_1.2.13-0_amd64.deb
 sudo dpkg -i libdvd-pkg_1.4.0-1-2_all.deb
 apt -f install
 dpkg-reconfigure libdvd-pkg
-apt install -y default-jre-headless
-apt install -y eject
+apt install default-jre-headless
+apt install eject
 
 echo -e "${RED}Installing ARM:Automatic Ripping Machine${NC}"
 cd /opt
@@ -88,7 +93,7 @@ git clone https://github.com/1337-server/automatic-ripping-machine.git arm
 chown -R arm:arm arm
 cd arm
 pip3 install -U setuptools
-apt install -y python3-dev python3-pip python3-venv python3-wheel
+apt install python3-dev python3-pip python3-venv python3-wheel
 pip3 install -U wheel
 pip3 install -r requirements.txt
 ln -s /opt/arm/setup/51-automedia.rules /lib/udev/rules.d/
