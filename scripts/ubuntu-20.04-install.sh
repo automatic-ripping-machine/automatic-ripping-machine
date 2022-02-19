c13438add02c38c7fc35edba3ace08ac93703a92
wolfy <wolfy@shitwolfymakes.com>
2022-02-18 18:16:03 -0700

    pycharm runs as "other", so the 777 is needed

--- scripts/ubuntu-20.04-install.sh
+++ scripts/ubuntu-20.04-install.sh
@@ -115,7 +115,7 @@ function install_arm_dev_env() {
     sudo ln -sf /opt/arm/arm.yaml /etc/arm/
 
     # all developer to write to the installation
-    sudo chmod -R 774 /opt/arm
+    sudo chmod -R 777 /opt/arm
 }
 
 function setup_autoplay() {
