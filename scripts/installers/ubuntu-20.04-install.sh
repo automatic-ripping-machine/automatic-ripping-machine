d638913dba20c404e3987fb5f509df8da9bec60e
wolfy <wolfy@shitwolfymakes.com>
2022-03-27 14:19:33 -0600

    added docker to dev installation

--- scripts/installers/ubuntu-20.04-install.sh
+++ scripts/installers/ubuntu-20.04-install.sh
@@ -63,6 +63,13 @@ function install_dev_requirements() {
     sudo apt install lsdvd -y
     sudo dpkg-reconfigure libdvd-pkg
     sudo apt install default-jre-headless -y
+
+    # install docker
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+    apt-cache policy docker-ce
+    sudo apt install docker-ce -y
 }
 
 function remove_existing_arm() {
