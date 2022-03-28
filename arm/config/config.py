9817a71edde74e523124a002d5b9f32745068d0c
wolfy <wolfy@shitwolfymakes.com>
2022-03-27 20:06:54 -0600

    refs new location of arm.yaml

--- arm/config/config.py
+++ arm/config/config.py
@@ -3,7 +3,7 @@
 import os
 import yaml
 
-yamlfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../..", "arm.yaml")
+yamlfile = os.path.join("/etc/arm/config", "arm.yaml")
 
 with open(yamlfile, "r") as f:
     try:
