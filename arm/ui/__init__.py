a6684ff0c817aaa7957f24b30ac516647250deee
wolfy <wolfy@shitwolfymakes.com>
2022-03-28 00:35:06 -0600

    refactored how the config.py data is accessed

--- arm/ui/__init__.py
+++ arm/ui/__init__.py
@@ -1,19 +1,19 @@
 import sys  # noqa: F401
 import os  # noqa: F401
 import bcrypt  # noqa: F401
+import arm.config.config as cfg
 
 from flask import Flask, logging, current_app  # noqa: F401
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-from arm.config.config import cfg
 from flask.logging import default_handler  # noqa: F401
 from getpass import getpass  # noqa: F401
 # import omdb
 
 from flask_login import LoginManager
 
-sqlitefile = 'sqlite:///' + arm_config['DBFILE']
+sqlitefile = 'sqlite:///' + cfg.arm_config['DBFILE']
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*", "send_wildcard": "False"}})
@@ -25,7 +25,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = sqlitefile
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # We should really gen a key for each system
 app.config['SECRET_KEY'] = "Big secret key"
-app.config['LOGIN_DISABLED'] = arm_config['DISABLE_LOGIN']
+app.config['LOGIN_DISABLED'] = cfg.arm_config['DISABLE_LOGIN']
 
 db = SQLAlchemy(app)
 
