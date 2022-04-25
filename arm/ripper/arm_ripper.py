e679afc7d8e56046a9152fdf11a48a76fe5b6699
1337-server <sndspamfilter@gmail.com>
2022-04-25 01:43:21 -0700

    fix quality checks

--- arm/ripper/arm_ripper.py
+++ arm/ripper/arm_ripper.py
@@ -7,8 +7,8 @@ import logging
 
 sys.path.append("/opt/arm")
 
-from arm.ripper import utils, makemkv, handbrake # noqa E402
-from arm.ui import app, db, constants # noqa E402
+from arm.ripper import utils, makemkv, handbrake  # noqa E402
+from arm.ui import app, db, constants  # noqa E402
 
 
 def rip_visual_media(have_dupes, job, logfile, protection):
