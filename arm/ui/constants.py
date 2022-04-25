0ebc8e189c06b46c2060301b0eaa80a45e158614
1337-server <sndspamfilter@gmail.com>
2022-04-24 21:26:19 -0700

    Work around added for Track 99 protection - ARM will now rip them using MakeMKV
    All the arm main.py sections, that relate to dvd/blu-ray moved to its own file.
    
    arm.ripper now uses the arm.ui.constants file
    
    jobs are now back(mostly) to using the saved config from the database
    
    job_dupe_check now only returns 1 value (True|False)
    
    More fstrings, comments, etc

--- arm/ui/constants.py
+++ arm/ui/constants.py
@@ -1,4 +1,5 @@
-"""ARM UI Constants"""
+"""ARM Constants"""
+# UI
 HOME_PAGE = '/index'
 ERROR_PAGE = 'error.html'
 ERROR_REDIRECT = "/error"
@@ -6,3 +7,6 @@ SETUP_STAGE_2 = '/setup'
 NO_ADMIN_ACCOUNT = "No admin account found"
 NO_JOB = "No job supplied"
 JSON_TYPE = "application/json"
+# Ripper
+NOTIFY_TITLE = "ARM notification"
+PROCESS_COMPLETE = " processing complete. "
