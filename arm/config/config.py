c0dceacf53d74306dc2ef3897f90f69f58e2ec60
Nick Hastings <nicholaschastings@gmail.com>
2022-06-08 08:46:45 +0900

    Adds optional config file locations
    First try reading config from ~/.config/arm/arm.yaml, then
    /etc/arm/arm.yaml before finally falling back to the arm.yaml in the
    source directory.

--- arm/config/config.py
+++ arm/config/config.py
@@ -3,10 +3,17 @@
 import os
 import yaml
 
-yamlfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../..", "arm.yaml")
+default_yamlfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../..", "arm.yaml")
 
-with open(yamlfile, "r") as f:
-    try:
-        cfg = yaml.load(f, Loader=yaml.FullLoader)
-    except Exception:
-        cfg = yaml.safe_load(f)  # For older versions use this
+for yamlfile in [os.path.expanduser('~/.config/arm/arm.yaml'),
+                 '/etc/arm/arm.yaml', default_yamlfile]:
+    if not os.path.exists(yamlfile):
+        continue
+
+    with open(yamlfile, "r") as f:
+        try:
+            cfg = yaml.load(f, Loader=yaml.FullLoader)
+        except Exception:
+            cfg = yaml.safe_load(f)  # For older versions use this
+
+        break
