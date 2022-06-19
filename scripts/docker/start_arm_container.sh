650c9ab5ef89fa5930891c61c62c31872f92945b
wolfy <wolfy@shitwolfymakes.com>
2022-06-19 16:07:54 -0600

    fixed order of statements

--- scripts/docker/start_arm_container.sh
+++ scripts/docker/start_arm_container.sh
@@ -3,11 +3,11 @@ docker run -d \
     -p "8080:8080" \
     -e ARM_UID="<id -u arm>" \
     -e ARM_GID="<id -g arm>" \
-    -v "/home/arm:<path_to_arm_user_home_folder>" \
-    -v "/home/arm/Music:<path_to_music_folder>" \
-    -v "/home/arm/logs:<path_to_logs_folder>" \
-    -v "/home/arm/media:<path_to_media_folder>" \
-    -v "/etc/arm/config:<path_to_config_folder>" \
+    -v "<path_to_arm_user_home_folder>/home/arm" \
+    -v "<path_to_music_folder>/home/arm/Music" \
+    -v "<path_to_logs_folder>:/home/arm/logs" \
+    -v "<path_to_media_folder>:/home/arm/media" \
+    -v "<path_to_config_folder>:/etc/arm/config" \
     --device="/dev/sr0:/dev/sr0" \
     --device="/dev/sr1:/dev/sr1" \
     --device="/dev/sr2:/dev/sr2" \
