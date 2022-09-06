5b62aa3759a26cc97d153030b1674206925e44f7
wolfy <wolfy@shitwolfymakes.com>
2022-09-05 19:33:32 -0600

    Fix all shellcheck warnings

--- scripts/thickclient/arm_wrapper.sh
+++ scripts/thickclient/arm_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -i
 
-set -e
+set -eo pipefail
 
 DEVNAME=$1
 PROTECTION=""
@@ -12,17 +12,20 @@ PROTECTION=""
 
 function parse_yaml {
    local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   local s='[[:space:]]*'
+   local w='[a-zA-Z0-9_]*'
+   local fs
+   fs=$(echo @ | tr @ '\034')
    sed -ne "s|^\($s\):|\1|" \
-        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+        -e "s|^\($s\)\($w\)$s:${s}[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  "$1" |
    awk -F"$fs" '{
       indent = length($1)/2;
       vname[indent] = $2;
       for (i in vname) {if (i > indent) {delete vname[i]}}
       if (length($3) > 0) {
          vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+         printf("%s%s%s=\"%s\"\n", "'"$prefix"'",vn, $2, $3);
       }
    }'
 }
