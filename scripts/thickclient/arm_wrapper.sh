f171fe74222f71814b894ebb29a64317b9a00831
microtechno9000 <microtechno@gmail.com>
2023-01-02 08:26:39 +1030

    uodated submodule

--- scripts/thickclient/arm_wrapper.sh
+++ scripts/thickclient/arm_wrapper.sh
@@ -4,6 +4,8 @@ set -eo pipefail
 
 DEVNAME=$1
 PROTECTION=""
+USER="mtech"
+
 #######################################################################################
 # YAML Parser to read Config
 #
@@ -39,35 +41,35 @@ eval "$(parse_yaml /etc/arm/config/arm.yaml "CONFIG_")"
 # ID_CDROM_MEDIA_CD = CD
 # ID_CDROM_MEDIA_DVD = DVD
 
-if [ "$ID_CDROM_MEDIA_DVD" == "1" ]; then
-  numtracks=$(lsdvd "/dev/${DEVNAME}" 2> /dev/null | sed 's/,/ /' | cut -d ' ' -f 2 | grep -E '[0-9]+' | sort -r | head -n 1)
-	if [ "$numtracks" == "99" ]; then
-	  if [ "$CONFIG_PREVENT_99" == "true" ]; then
-		  echo "[ARM] ${DEVNAME} has 99 Track Protection...Ripping 99 is disabled.. Ejecting disc." | logger -t ARM -s
-			eject "$DEVNAME"
-		else
-			echo "[ARM] ${DEVNAME} has 99 Track Protection...Trying workaround" | logger -t ARM -s
-			PROTECTION="-p 1"
-		fi
-	fi
-	echo "[ARM] Starting ARM for DVD on ${DEVNAME}" | logger -t ARM -s
+#if [ "$ID_CDROM_MEDIA_DVD" == "1" ]; then
+#  numtracks=$(lsdvd "/dev/${DEVNAME}" 2> /dev/null | sed 's/,/ /' | cut -d ' ' -f 2 | grep -E '[0-9]+' | sort -r | head -n 1)#
+#	if [ "$numtracks" == "99" ]; then
+#	  if [ "$CONFIG_PREVENT_99" == "true" ]; then
+#		  echo "[ARM] ${DEVNAME} has 99 Track Protection...Ripping 99 is disabled.. Ejecting disc." | logger -t ARM -s
+#			eject "$DEVNAME"
+#		else
+#			echo "[ARM] ${DEVNAME} has 99 Track Protection...Trying workaround" | logger -t ARM -s
+#			PROTECTION="-p 1"
+#		fi
+#	fi
+#	echo "[ARM] Starting ARM for DVD on ${DEVNAME}" | logger -t ARM -s
+#
+#elif [ "$ID_CDROM_MEDIA_BD" == "1" ]; then
+#	echo "[ARM] Starting ARM for Blu-ray on ${DEVNAME}" | logger -t ARM -s
+#
+#elif [ "$ID_CDROM_MEDIA_CD" == "1" ]; then
+#	echo "[ARM] Starting ARM for CD on ${DEVNAME}" | logger -t ARM -s
+#
+#elif [ "$ID_FS_TYPE" != "" ]; then
+#	echo "[ARM] Starting ARM for Data Disk on ${DEVNAME} with File System ${ID_FS_TYPE}" | logger -t ARM -s
+#
+#else
+#	echo "[ARM] Not CD, Bluray, DVD or Data. Bailing out on ${DEVNAME}" | logger -t ARM -s
+#	exit #bail out
+#
+#fi
 
-elif [ "$ID_CDROM_MEDIA_BD" == "1" ]; then
-	echo "[ARM] Starting ARM for Blu-ray on ${DEVNAME}" | logger -t ARM -s
-
-elif [ "$ID_CDROM_MEDIA_CD" == "1" ]; then
-	echo "[ARM] Starting ARM for CD on ${DEVNAME}" | logger -t ARM -s
-
-elif [ "$ID_FS_TYPE" != "" ]; then
-	echo "[ARM] Starting ARM for Data Disk on ${DEVNAME} with File System ${ID_FS_TYPE}" | logger -t ARM -s
-
-else
-	echo "[ARM] Not CD, Bluray, DVD or Data. Bailing out on ${DEVNAME}" | logger -t ARM -s
-	exit #bail out
-
-fi
-
-/bin/su -l -c "echo /usr/bin/python3 /opt/arm/arm/ripper/main.py -d ${DEVNAME} ${PROTECTION} | at now" -s /bin/bash arm
+/bin/su -l -c "echo /usr/bin/python3 /opt/arm/arm/ripper/main.py -d ${DEVNAME} ${PROTECTION} | at now" -s /bin/bash ${USER}
 
 #######################################################################################
 # Check to see if the admin page is running, if not, start it
@@ -75,5 +77,5 @@ fi
 
 if ! pgrep -f "runui.py" > /dev/null; then
 	echo "[ARM] ARM Webgui not running; starting it " | logger -t ARM -s
-	/bin/su -l -c "/usr/bin/python3 /opt/arm/arm/runui.py  " -s /bin/bash arm
+	/bin/su -l -c "/usr/bin/python3 /opt/arm/arm/runui.py  " -s /bin/bash ${USER}
 fi
