721a42245bb837ded5d29a8808c88773fe22f177
Microtechno9000 <microtechno@gmail.com>
2023-01-06 07:00:21 +1030

    reverted changes made to arm_wrapper
    changes made to wrapper during testing, and accidentally pulled into a commit

--- scripts/thickclient/arm_wrapper.sh
+++ scripts/thickclient/arm_wrapper.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 DEVNAME=$1
 PROTECTION=""
-USER="mtech"
+USER="arm"
 
 #######################################################################################
 # YAML Parser to read Config
@@ -37,37 +37,37 @@ eval "$(parse_yaml /etc/arm/config/arm.yaml "CONFIG_")"
 #######################################################################################
 # Log Discovered Type and Start Rip
 #######################################################################################
-# ID_CDROM_MEDIA_BD = Blu-ray
-# ID_CDROM_MEDIA_CD = CD
-# ID_CDROM_MEDIA_DVD = DVD
+ID_CDROM_MEDIA_BD = Blu-ray
+ID_CDROM_MEDIA_CD = CD
+ID_CDROM_MEDIA_DVD = DVD
 
-#if [ "$ID_CDROM_MEDIA_DVD" == "1" ]; then
-#  numtracks=$(lsdvd "/dev/${DEVNAME}" 2> /dev/null | sed 's/,/ /' | cut -d ' ' -f 2 | grep -E '[0-9]+' | sort -r | head -n 1)#
-#	if [ "$numtracks" == "99" ]; then
-#	  if [ "$CONFIG_PREVENT_99" == "true" ]; then
-#		  echo "[ARM] ${DEVNAME} has 99 Track Protection...Ripping 99 is disabled.. Ejecting disc." | logger -t ARM -s
-#			eject "$DEVNAME"
-#		else
-#			echo "[ARM] ${DEVNAME} has 99 Track Protection...Trying workaround" | logger -t ARM -s
-#			PROTECTION="-p 1"
-#		fi
-#	fi
-#	echo "[ARM] Starting ARM for DVD on ${DEVNAME}" | logger -t ARM -s
-#
-#elif [ "$ID_CDROM_MEDIA_BD" == "1" ]; then
-#	echo "[ARM] Starting ARM for Blu-ray on ${DEVNAME}" | logger -t ARM -s
-#
-#elif [ "$ID_CDROM_MEDIA_CD" == "1" ]; then
-#	echo "[ARM] Starting ARM for CD on ${DEVNAME}" | logger -t ARM -s
-#
-#elif [ "$ID_FS_TYPE" != "" ]; then
-#	echo "[ARM] Starting ARM for Data Disk on ${DEVNAME} with File System ${ID_FS_TYPE}" | logger -t ARM -s
-#
-#else
-#	echo "[ARM] Not CD, Bluray, DVD or Data. Bailing out on ${DEVNAME}" | logger -t ARM -s
-#	exit #bail out
-#
-#fi
+if [ "$ID_CDROM_MEDIA_DVD" == "1" ]; then
+ numtracks=$(lsdvd "/dev/${DEVNAME}" 2> /dev/null | sed 's/,/ /' | cut -d ' ' -f 2 | grep -E '[0-9]+' | sort -r | head -n 1)#
+	if [ "$numtracks" == "99" ]; then
+	  if [ "$CONFIG_PREVENT_99" == "true" ]; then
+		  echo "[ARM] ${DEVNAME} has 99 Track Protection...Ripping 99 is disabled.. Ejecting disc." | logger -t ARM -s
+			eject "$DEVNAME"
+		else
+			echo "[ARM] ${DEVNAME} has 99 Track Protection...Trying workaround" | logger -t ARM -s
+			PROTECTION="-p 1"
+		fi
+	fi
+	echo "[ARM] Starting ARM for DVD on ${DEVNAME}" | logger -t ARM -s
+
+elif [ "$ID_CDROM_MEDIA_BD" == "1" ]; then
+	echo "[ARM] Starting ARM for Blu-ray on ${DEVNAME}" | logger -t ARM -s
+
+elif [ "$ID_CDROM_MEDIA_CD" == "1" ]; then
+	echo "[ARM] Starting ARM for CD on ${DEVNAME}" | logger -t ARM -s
+
+elif [ "$ID_FS_TYPE" != "" ]; then
+	echo "[ARM] Starting ARM for Data Disk on ${DEVNAME} with File System ${ID_FS_TYPE}" | logger -t ARM -s
+
+else
+	echo "[ARM] Not CD, Bluray, DVD or Data. Bailing out on ${DEVNAME}" | logger -t ARM -s
+	exit #bail out
+
+fi
 
 /bin/su -l -c "echo /usr/bin/python3 /opt/arm/arm/ripper/main.py -d ${DEVNAME} ${PROTECTION} | at now" -s /bin/bash ${USER}
 
