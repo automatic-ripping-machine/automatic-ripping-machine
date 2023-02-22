f59618d0019d224c67b0b73733903206f74a01c0
wolfy <wolfy@shitwolfymakes.com>
2023-02-22 01:13:55 -0700

    Add update logic

--- scripts/update_key.sh
+++ scripts/update_key.sh
@@ -6,26 +6,39 @@
 # beta key.
 # Link: https://forum.makemkv.com/forum/viewtopic.php?p=119221#p119221
 
+MAKEMKV_PERMA_KEY=
+if [ -n "$1" ]; then
+    MAKEMKV_PERMA_KEY=$1
+fi
+
 makemkv_serial_url="https://forum.makemkv.com/forum/viewtopic.php?f=5&t=1053"
 makemkv_serial=$(curl -fsSL "$makemkv_serial_url" | grep -oP 'T-[\w\d@]{66}')
 echo "MakeMKV beta key for this month: $makemkv_serial"
-# TODO: implement this
-# if file exists OR grep doesn't find key string in settings
-SETTINGS_FILE="/home/arm/.MakeMKV/settings.conf"
-if [[ -f $SETTINGS_FILE ]] || grep -q "^app_Key.*" "$SETTINGS_FILE"; then
-    echo "Either $SETTINGS_FILE doesn't exist, or app_Key is not inside it"
-#     if run w/arg
-#         append permakey string to settings
-#     else
-#         append beta key to settings
-# else
-#     if run w/arg
-#         sed replace key in settings w/permakey
-#     else
-#         sed replace key is settings w/beta key
-fi
 
 mkdir -p "/home/arm/.MakeMKV"
 chown arm:arm "/home/arm/.MakeMKV"
-echo "app_Key = \"$makemkv_serial\"" > /home/arm/.MakeMKV/settings.conf
+
+# if file exists OR grep doesn't find key string in settings
+SETTINGS_FILE="/home/arm/.MakeMKV/settings.conf"
+if [[ -f $SETTINGS_FILE ]] || ! grep -q "app_Key" "$SETTINGS_FILE"; then
+    echo "Either $SETTINGS_FILE doesn't exist, or app_Key is not inside it"
+    # if run w/arg
+    if [ -n "$MAKEMKV_PERMA_KEY" ]; then
+        # append permakey string to settings
+        echo "app_Key = \"$MAKEMKV_PERMA_KEY\"" > "/home/arm/.MakeMKV/settings.conf"
+    else
+        # append beta key to settings
+        echo "app_Key = \"$makemkv_serial\"" > /home/arm/.MakeMKV/settings.conf
+    fi
+else
+    # if run w/arg
+    if [ -n "$MAKEMKV_PERMA_KEY" ]; then
+        # sed replace key in settings w/permakey
+        sed -i "s|app_Key = \"T-.*\"|app_Key = \"$MAKEMKV_PERMA_KEY\"|" $SETTINGS_FILE
+    else
+        # sed replace key is settings w/beta key
+        sed -i "s|app_Key = \"T-.*\"|app_Key = \"$makemkv_serial\"|" $SETTINGS_FILE
+    fi
+fi
+
 chown arm:arm "/home/arm/.MakeMKV/settings.conf"
