796f70e28e82f0c98e9fe2c1b5c5b8781640bbed
wolfy <wolfy@shitwolfymakes.com>
2023-02-22 17:57:49 -0700

    fixed logical bug

--- scripts/update_key.sh
+++ scripts/update_key.sh
@@ -6,32 +6,39 @@
 # beta key.
 # Link: https://forum.makemkv.com/forum/viewtopic.php?p=119221#p119221
 
+makemkv_serial_url="https://forum.makemkv.com/forum/viewtopic.php?f=5&t=1053"
 MAKEMKV_PERMA_KEY=
+
+# save passed MAKEMKV_PERMA_KEY or scrape this month's beta key
 if [ -n "$1" ]; then
     echo "MAKEMKV_PERMA_KEY passed as arg"
     MAKEMKV_PERMA_KEY=$1
+else
+    makemkv_serial=$(curl -fsSL "$makemkv_serial_url" | grep -oP 'T-[\w\d@]{66}')
+    echo "MakeMKV beta key for this month: $makemkv_serial"
 fi
 
-makemkv_serial_url="https://forum.makemkv.com/forum/viewtopic.php?f=5&t=1053"
-makemkv_serial=$(curl -fsSL "$makemkv_serial_url" | grep -oP 'T-[\w\d@]{66}')
-echo "MakeMKV beta key for this month: $makemkv_serial"
+# create .MakeMKV dir if it doesn't already exist
+MAKEMKV_DIR="/home/arm/.MakeMKV"
+if [ ! -d "$MAKEMKV_DIR" ]; then
+    mkdir -p "$MAKEMKV_DIR"
+    chown arm:arm "$MAKEMKV_DIR"
+fi
 
-mkdir -p "/home/arm/.MakeMKV"
-chown arm:arm "/home/arm/.MakeMKV"
-
-# if file exists OR grep doesn't find key string in settings
-SETTINGS_FILE="/home/arm/.MakeMKV/settings.conf"
-if [[ -f "$SETTINGS_FILE" ]] || ! grep -q "app_Key" "$SETTINGS_FILE"; then
+# if file doesn't exist OR grep doesn't find key string in settings
+SETTINGS_FILE="$MAKEMKV_DIR/settings.conf"
+if [[ ! -f "$SETTINGS_FILE" ]] || ! grep -q "app_Key" "$SETTINGS_FILE"; then
     echo "Either $SETTINGS_FILE doesn't exist, or app_Key is not inside it"
     # if run w/arg
     if [ -n "$MAKEMKV_PERMA_KEY" ]; then
         # append permakey string to settings
-        echo "app_Key = \"$MAKEMKV_PERMA_KEY\"" >> "/home/arm/.MakeMKV/settings.conf"
+        echo "app_Key = \"$MAKEMKV_PERMA_KEY\"" >> "$SETTINGS_FILE"
     else
         # append beta key to settings
-        echo "app_Key = \"$makemkv_serial\"" >> "/home/arm/.MakeMKV/settings.conf"
+        echo "app_Key = \"$makemkv_serial\"" >> "$SETTINGS_FILE"
     fi
 else
+    echo "$SETTINGS_FILE exists, updating value of app_Key"
     # if run w/arg
     if [ -n "$MAKEMKV_PERMA_KEY" ]; then
         # sed replace key in settings w/permakey
