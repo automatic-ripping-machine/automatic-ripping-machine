ce3d977d48dc6296dab673c2a1c52c11ff833ed4
wrjacqmein <wrjacqmein@gmail.com>
2023-05-04 08:47:59 -0400

    Update arm_user_files_setup.sh
    updated to link version of abcde with docker passed file.

--- scripts/docker/runit/arm_user_files_setup.sh
+++ scripts/docker/runit/arm_user_files_setup.sh
@@ -61,5 +61,5 @@ done
     
     # abcde.conf is expected in /etc by the abcde installation
     cp --no-clobber "/opt/arm/setup/.abcde.conf" "/etc/.abcde.conf"
-    ln -sf /etc/.abcde.conf /etc/arm/config/abcde.conf
+    ln -sf /etc/arm/config/abcde.conf /etc/.abcde.conf
     chown arm:arm "/etc/.abcde.conf" "/etc/arm/config/abcde.conf"
