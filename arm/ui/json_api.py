43253f0d281b243bf3d8c28d0092371b21058f72
Microtechno9000 <microtechno@gmail.com>
2023-05-29 21:38:37 +0930

    unknown audio fix
    fixed bug with api reporting unknown int

--- arm/ui/json_api.py
+++ arm/ui/json_api.py
@@ -179,7 +179,9 @@ def process_audio_logfile(logfile, job, job_results):
                 job.progress = round(percentage(job_stage_index.group(1), job.no_of_titles + 1))
                 job.progress_round = round(job.progress)
             except Exception as error:
-                job.stage = f"Unknown -  {error}"
+                app.logger.debug("Error processing abcde logfile. Error dump" \
+                                 f"-  {error}")
+                job.stage = "Unknown"
                 job.eta = "Unknown"
                 job.progress = job.progress_round = 0
     return job_results
