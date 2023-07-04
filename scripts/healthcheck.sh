5d9df54834d5fbed9730e41727247037288e6c59
doudalidou <jeanyves.drolet@gmail.com>
2023-07-04 12:55:55 -0400

    first draft

---
+++ scripts/healthcheck.sh
@@ -0,0 +1,5 @@
+curl --fail "http://$(hostname -i):8080" || exit 1
+ps -e | pgrep systemd-udevd || exit 1
+makemkvcon | grep www.makemkv.com/developers || exit 1
+HandBrakeCLI --version || exit 1
+abcde -v || exit 1
