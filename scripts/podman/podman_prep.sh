dcac29d364afab6517dca74388ec95a74ea78d84
Adam Clater <aclater@gmail.com>
2024-12-05 13:04:38 -0500

    Update podman_prep.sh
    Add start arm. Will fail if less than 4 cdroms present.

--- scripts/podman/podman_prep.sh
+++ scripts/podman/podman_prep.sh
@@ -17,3 +17,7 @@ chown -R arm:arm ~arm
 # Update firewall-d to allow port 8080
 firewall-cmd --permanent --add-port=8080/tcp
 firewall-cmd --reload
+
+# Start arm - this depends on 4 CDROM Devices - sr0-sr03, if not there, it will fail.
+systemctl --user --machine=arm@ daemon-reload
+systemctl --user --machine=arm@ start arm
