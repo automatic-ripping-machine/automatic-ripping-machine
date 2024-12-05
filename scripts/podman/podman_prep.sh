d6300258be0fa34806e9ac6ae01aff08477297fd
aclater <aclater@gmail.com>
2024-12-05 11:47:45 -0500

    Added script to install podman, configure arm user and set firewall rule

---
+++ scripts/podman/podman_prep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# First lets make sure podman is installed
+dnf -yq install podman
+
+# Now we'll creat our arm user and add them to cdrom and render groups
+useradd arm -G cdrom,render
+
+# Give arm user SystemD linger - allows processes to run after their session ends
+loginctl enable-linger arm
+
+# Create arm and SystemD container directories
+mkdir -p ~arm/arm/{config,logs,media,music} ~arm/.config/containers/systemd
+chown -R arm:arm ~arm
+
+# Update firewall-d to allow port 8080
+firewall-cmd --permanent --add-port=8080/tcp
+firewall-cmd --reload
