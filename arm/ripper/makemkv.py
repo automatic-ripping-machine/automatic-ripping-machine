40a305517a074d4b6a5771571a0ac37e41d9a359
Matthias Kastner <16167358+ukos-git@users.noreply.github.com>
2025-01-04 14:26:47 +0100

    Readability

--- arm/ripper/makemkv.py
+++ arm/ripper/makemkv.py
@@ -537,7 +537,7 @@ def makemkv_info(job, select=None, index=9999, options=None):
     max_processes = job.config.MAX_CONCURRENT_MAKEMKVINFO
     job.status = JOB_STATUS_WAITING
     db.session.commit()
-    utils.sleep_check_process("makemkvcon", max_processes, (10, wait_time, 10))
+    utils.sleep_check_process("makemkvcon", max_processes, sleep=(10, wait_time, 10))
     job.status = JOB_STATUS_INFO
     db.session.commit()
     try:
@@ -552,7 +552,7 @@ def makemkv_info(job, select=None, index=9999, options=None):
             # give other processes time to use this function.
             sleep(wait_time)
         # sleep here until all processes finish (hopefully)
-        utils.sleep_check_process("makemkvcon", max_processes, (wait_time, wait_time + 1, 1))
+        utils.sleep_check_process("makemkvcon", max_processes, sleep=(wait_time, wait_time + 1, 1))
         job.status = JOB_STATUS_RIPPING
         db.session.commit()
 
