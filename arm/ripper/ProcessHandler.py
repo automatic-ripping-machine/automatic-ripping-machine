ee5ec289a4d1b2fa1dd22b08c1dd2a85aadd9009
xieve <2876434-xieve@users.noreply.gitlab.com>
2025-04-01 00:45:22 +0200

    feat(ProcessHandler): decode by default

--- arm/ripper/ProcessHandler.py
+++ arm/ripper/ProcessHandler.py
@@ -2,26 +2,41 @@
 Function definition
   Wrapper for the python subprocess module
 """
+
 import logging
 import subprocess
+from typing import Optional, List
 
 
-def arm_subprocess(cmd, in_shell, check=False):
+def arm_subprocess(cmd: str | List[str], shell=False, check=False) -> Optional[str]:
     """
-    Handle creating new subprocesses and catch any errors
+    Spawn blocking subprocess
+
+    :param cmd: Command to run
+    :param shell: Run ``cmd`` in a shell
+    :param check: Raise ``CalledProcessError`` if ``cmd`` returns non-zero exit code
+
+    :return: Output of ``cmd``, or ``None`` if it returned a non-zero exit code
+
+    :raise CalledProcessError:
     """
     arm_process = None
     logging.debug(f"Running command: {cmd}")
     try:
         arm_process = subprocess.check_output(
-            cmd,
-            shell=in_shell
+            cmd, shell=shell, encoding="utf-8"
         )
-    except subprocess.CalledProcessError as error:
-        logging.error(error)
-        logging.error("Output was:")
-        logging.error(error.output)
-        logging.debug(str(error.__traceback__))
+    except (subprocess.CalledProcessError, OSError) as error:
+        decoded_output = error.output.strip()
+        logging.error(
+            f"Error while running command: {cmd}\n"
+            + (
+                f"Output was: {decoded_output}"
+                if decoded_output
+                else "The command produced no output."
+            ),
+            exc_info=error,
+        )
         if check:
             raise error
 
