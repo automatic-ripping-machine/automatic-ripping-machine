93bd196a624d7815739cc2cd72ab43882837603f
flammableliquids <geforbes@gmail.com>
2025-05-11 09:17:40 +1000

    - new form validation function, which validates if umasks for chmod command to see if they are valid. - SettingsForm() function modified to read ripperSettingsConfig, and then use that to establish what field types are required, and their validators. - - needs modification to funtion with comments.json -  quickJsonGenerator generates ripperFormCOnfig based on arm.yaml and comments.json, not intended for production use, just to regenerate the ripperFormCOnfig - ripperFormConfig.json is intended to be a list of all fields in the ripper form, and the related field types and their validations. - - Comments.json will still be used to define the comments (keeping that central) - - default value, and comment shoudl probably be removed from the field for continuity. They are included in the file at generation to make it easier to define validations and field types. * this commit isn't working 100%, it is being made to ensure that I have a rollbackpoint during develop *

--- arm/ui/forms.py
+++ arm/ui/forms.py
@@ -7,10 +7,25 @@ from wtforms.validators import DataRequired, ValidationError, IPAddress, Optiona
 from os import path
 import arm.config.config as cfg
 from arm.ui import app
+import json
 
-def validate_path_exists(field):
+def validate_path_exists(form, field):
     if not path.exists(field.data):
         raise ValidationError(f"The path specified does not exist:\r\n {field}")
+
+def validate_umask(form, field):
+    value = field.data
+    try:
+        # Accept both '002' and '0o002' formats
+        if value.startswith("0o"):
+            umask_int = int(value, 8)
+        else:
+            umask_int = int(value, 8)
+
+        if not (0 <= umask_int <= 0o777):
+            raise ValidationError("Umask must be a valid octal between 000 and 777.")
+    except ValueError:
+        raise ValidationError("Invalid octal number format.")
 
 class TitleSearchForm(FlaskForm):
     """Main title search form used on pages\n
@@ -29,57 +44,104 @@ class ChangeParamsForm(FlaskForm):
     DISCTYPE = SelectField('Disc Type: ', choices=[('dvd', 'DVD'), ('bluray', 'Blu-ray'),
                                                    ('music', 'Music'), ('data', 'Data')])
     # "music", "dvd", "bluray" and "data"
-    MAINFEATURE = BooleanField('Main Feature: ', DataRequired)
+    MAINFEATURE = BooleanField('Main Feature: ', DataRequired())
     MINLENGTH = IntegerField('Minimum Length: ', DataRequired())
     MAXLENGTH = IntegerField('Maximum Length: ', DataRequired())
     submit = SubmitField('Submit')
 
-
-def SettingsForm(dictFormFields:dict, comments ={}):
+ripperSettingsConfigFile = '/opt/arm/arm/ui/ripperFormConfig.json'
+def SettingsForm(postRequestDict=None):
     class SettingsForm(FlaskForm):
         submit = SubmitField('Submit')
-        
+    
+    with open(ripperSettingsConfigFile, 'r') as ripperFormConfig:
+        dictFormFields = json.load(ripperFormConfig)
+    # request.form.to_dict() returns a dict with the form fields and the value. All as strings on empty.
+    # so we need to return a form with default values populated
+    # postRequestDict
+
     for key, value in dictFormFields.items():
+        if postRequestDict is not None:
+            if key == 'csrf_token':
+                # Skip the CSRF token field
+                continue
         #Infer the type of form field based on the value type
         app.logger.debug(f"Inferring form field type for {key}: {type(value)}")
-        # Just in case the comment is empty, we will get the value for it, and
-        # if none, set it to string
-        comm_value = comments.get(key)
-        if comm_value is None: comm_value = ""
+        # ripperFormConfig should have a dict per field: default, comment, dataValidation, formFieldType
+        commentValue = dictFormFields[key]["comment"]
+        if commentValue is None: commentValue = ""
+        if postRequestDict is not None:
+            fieldDefault = postRequestDict[key]
+        else:
+            fieldDefault = dictFormFields[key]["default"]
+        if commentValue is None: commentValue = ""
+        fieldType = dictFormFields[key]["formFieldType"]
+        # The next is a bit tricky, getting a list of data validations, setting them up as objects
+        # or functions depending on what was passed
+        if isinstance(dictFormFields[key]['dataValidation'], list) and len(dictFormFields[key]['dataValidation']) > 0:
+            validators = dictFormFields[key]['dataValidation']
+            for x in validators:
+                if x == "validate_path_exists":
+                    validators.remove(x)
+                    validators.append(validate_path_exists)
+                elif x == "validate_umask":
+                    validators.remove(x)
+                    validators.append(validate_umask)
+                else:
+                    try:
+                        n_v_c = globals()[x]
+                        n_v_c = n_v_c()
+                        validators.remove(x)
+                        validators.append(n_v_c)
+                    except Exception as e:
+                        app.logger.warning(f"Error adding validator {x} to {key}: {e}")
+        else:
+            validators = None
         
-        if isinstance(value, bool):
-            f = BooleanField(label=key.replace("_", " "),
-                        description=comm_value,
-                        default=value,
-                        render_kw={'title':comm_value}
+
+        if isinstance(fieldDefault, bool) and fieldType == "SelectField":
+            f = SelectField(label=key.replace("_", " "),
+                        description=commentValue,
+                        default=str(fieldDefault).title(),
+                        render_kw={'title':commentValue},
+                        validators=validators,
+                        choices=[
+                            ('True', 'True'),
+                            ('False', 'False')
+                            ],
                         )
-        elif isinstance(value, int):
+        elif fieldType == "SelectField":
+            paired_list = [(x, x) for x in fieldDefault]
+            f = SelectField(label=key.replace("_", " "),
+                        description=commentValue,
+                        # default=str(fieldDefault),
+                        render_kw={'title':commentValue},
+                        validators=validators,
+                        choices=paired_list,
+                        )
+        elif fieldType == "IntegerField":
             f = IntegerField(label=key.replace("_", " "),
-                        description=comm_value,
-                        default=value,
-                        render_kw={'title':comm_value}
+                        description=commentValue,
+                        default=int(fieldDefault),
+                        validators=validators,
+                        render_kw={'title':commentValue}
                         )
-        elif isinstance(value, float):
+        elif fieldType == "FloatField":
             f = FloatField(label=key.replace("_", " "),
-                        description=comm_value,
-                        default=value,
-                        render_kw={'title':comm_value}
+                        description=commentValue,
+                        default=float(fieldDefault),
+                        validators=validators,
+                        render_kw={'title':commentValue}
                         )
-        elif isinstance(value, str):
+        elif fieldType == "StringField":
             f = StringField(label=key.replace("_", " "),
-                        description=comm_value,
-                        default=value,
-                        render_kw={'title':comm_value}
+                        description=commentValue,
+                        default=str(fieldDefault),
+                        validators=validators,
+                        render_kw={'title':commentValue}
                         )
-            # if "binary to call" in comments[key] or "Path to" in comments[key]:
-                # f.validators([DataRequired(),validate_path_exists()])
         else:
             app.logger.warning(f"Unknown type for {key}: {type(value)}, returning StringField")
-            f = StringField(label=key.replace("_", " "),
-                        description=comm_value,
-                        default=str(value),
-                        render_kw={'title':comm_value}
-                        )  # fallback
         setattr(SettingsForm, key, f)
     return SettingsForm()
 
