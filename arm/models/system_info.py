6841dad38daa3c0bdf17234ef1bfebab1b4268ed
Microtechno9000 <microtechno@gmail.com>
2025-05-25 17:01:02 +0930

    Bugfix - CPU Info filtering from /cat/proc/cpuinfo

--- arm/models/system_info.py
+++ arm/models/system_info.py
@@ -2,6 +2,7 @@ import platform
 import psutil
 import re
 import subprocess
+import logging
 
 from arm.ui import db
 
@@ -27,37 +28,43 @@ class SystemInfo(db.Model):
         function to collect and return some cpu info
         ideally want to return {name} @ {speed} Ghz
         """
-        self.cpu = "Unknown"
+        self.cpu = "Unable to Identify"
+        logging.debug("****** Getting CPU Info ******")
+
         if platform.system() == "Windows":
             self.cpu = platform.processor()
         elif platform.system() == "Darwin":
             self.cpu = subprocess.check_output(['/usr/sbin/sysctl', "-n", "machdep.cpu.brand_string"]).strip()
         elif platform.system() == "Linux":
             command = "cat /proc/cpuinfo"
-            fulldump = str(subprocess.check_output(command, shell=True).strip())
+            # fulldump = subprocess.check_output(command, shell=True).strip()
+            fulldump = subprocess.check_output(command, shell=True).decode()
+            # logging.debug(f"Full cpuinfo: {fulldump}")
+
             # Take any float trailing "MHz", some whitespace, and a colon.
-            speeds = re.search(r"\\nmodel name\\t:.*?GHz\\n", fulldump)
-            if speeds:
-                # We have intel CPU
-                speeds = str(speeds.group())
-                speeds = speeds.replace('\\n', ' ')
-                speeds = speeds.replace('\\t', ' ')
-                speeds = speeds.replace('model name :', '')
-                self.cpu = speeds
+            # like: model name  : Intel(R) Celeron(R) G4930T CPU @ 3.00GHz
+            intel_match = re.search(r"model name\s*:\s*(.*?GHz)", fulldump)
+            logging.debug(f"Intel output: {intel_match}")
+            if intel_match:
+                self.cpu = intel_match.group(1)
+
             # AMD CPU
-            amd_name_full = re.search(r"model name\\t: (.*?)\\n", fulldump)
+            amd_name_full = re.search(r"model name\s*:\s*(.*)", fulldump)
+            logging.debug(f"AMD output: {amd_name_full}")
             if amd_name_full:
                 amd_name = amd_name_full.group(1)
-                amd_mhz = re.search(r"cpu MHz(?:\\t)*: ([.0-9]*)\\n", fulldump)  # noqa: W605
+                amd_mhz = re.search(r"cpu MHz\s*:\s*([.0-9]+)", fulldump)  # noqa: W605
                 if amd_mhz:
                     amd_ghz = round(float(amd_mhz.group(1)) / 1000, 2)  # this is a good idea
                     self.cpu = str(amd_name) + " @ " + str(amd_ghz) + " GHz"
+
             # ARM64 CPU
             arm_cpu = re.search(r"\\nmodel name\\t:(.*?)\\n", fulldump)
+            logging.debug(f"ARM64 output: {arm_cpu}")
             if arm_cpu:
                 self.cpu = str(arm_cpu.group(1))[:19]
-        else:
-            self.cpu = "N/A"
+
+        logging.debug("****************************")
 
     def get_memory(self):
         """ get the system total memory """
