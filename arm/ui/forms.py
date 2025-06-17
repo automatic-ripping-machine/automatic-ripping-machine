98827abbba0004121e1113dc5a2e9a531a229a0e
flammableliquids <geforbes@gmail.com>
2025-06-17 23:18:23 +1000

    form custom vaildators move to different file. RipperSettings no longer tries to set default values, that caused complexity problems when trying to open an existign config file and save it.

--- arm/ui/forms.py
+++ arm/ui/forms.py
@@ -1,52 +1,16 @@
-from typing import Any, List
+from typing import List
 """Forms used in the arm ui"""
 from flask_wtf import FlaskForm
 from wtforms import Form, Field, StringField, SubmitField, SelectField, \
-    IntegerField, BooleanField, PasswordField, Form, FieldList, \
-    FormField, HiddenField, FloatField, IntegerRangeField
-from wtforms.validators import DataRequired, ValidationError, IPAddress, InputRequired, Optional
+    IntegerField, BooleanField, PasswordField, FieldList, \
+    FormField, HiddenField, FloatField, RadioField, IntegerRangeField
+from wtforms.validators import DataRequired, ValidationError, IPAddress, InputRequired
 from os import path
 import arm.config.config as cfg
 from arm.ui import app
 import arm.ui.utils as ui_utils
-
 ### Custom Validators
-
-def validate_path_exists(form:Form, field:Field):
-    if not path.exists(field.data):
-        raise ValidationError(f"The path specified does not exist:\r\n {field}")
-
-def validate_umask(form:Form, field:Field):
-    value = field.data
-    try:
-        # Accept both '002' and '0o002' formats
-        if value.startswith("0o"):
-            umask_int = int(value, 8)
-        else:
-            umask_int = int(value, 8)
-
-        if not (0 <= umask_int <= 0o777):
-            raise ValidationError("Umask must be a valid octal between 000 and 777.")
-    except ValueError:
-        raise ValidationError("Invalid octal number format.")
-
-def validate_non_manditory_string(form:Form, field:Field):
-    originalLength = len(field.data)
-    if originalLength > 0:
-        text = field.data.replace('<p>','').replace('</p>','').replace('&nbsp;','')\
-                         .replace('&ensp;','').replace('&emsp;','').replace('<br>','')
-        if len(text) == 0:
-            raise ValidationError("Field must not contain only HTML tags.")
-        # check for non-ASCII characters
-        if not all(ord(c) < 128 for c in text):
-            raise ValidationError("Field must not contain non-ASCII characters.")
-        # check for non-printable characters
-        if not all(c.isprintable() for c in text):
-            raise ValidationError("Field must not contain non-printable characters.")
-        # remove whitespace
-        text = text.strip().replace("\t","").replace("\n","").replace("\r","").replace("\f","").replace("\v","")
-        if len(text) == 0:
-            raise ValidationError("Field must not contain only whitespace.")
+from .forms_custom_validators import validate_path_exists, validate_umask, validate_non_manditory_string
 
 
 class TitleSearchForm(FlaskForm):
@@ -146,7 +110,7 @@ def SettingsForm() -> FlaskForm:
         if isinstance(fieldDefault, bool) and fieldType == "SelectField":
             f = SelectField(label=key.replace("_", " "),
                         description=commentValue,
-                        default=str(fieldDefault).title(),
+                        # default=str(fieldDefault).title(),
                         render_kw={'title':commentValue},
                         validators=validators,
                         choices=[
@@ -157,7 +121,7 @@ def SettingsForm() -> FlaskForm:
         elif isinstance(fieldDefault, bool) and fieldType == "RadioField":
             f = RadioField(label=key.replace("_", " "),
                         description=commentValue,
-                        default=str(fieldDefault).title(),
+                        # default=str(fieldDefault).title(),
                         render_kw={'title':commentValue},
                         validators=validators,
                         choices=[
@@ -194,21 +158,21 @@ def SettingsForm() -> FlaskForm:
         elif fieldType == "IntegerField":
             f = IntegerField(label=key.replace("_", " "),
                         description=commentValue,
-                        default=int(fieldDefault),
+                        # default=int(fieldDefault),
                         validators=validators,
                         render_kw={'title':commentValue}
                         )
         elif fieldType == "FloatField":
             f = FloatField(label=key.replace("_", " "),
                         description=commentValue,
-                        default=float(fieldDefault),
+                        # default=float(fieldDefault),
                         validators=validators,
                         render_kw={'title':commentValue}
                         )
         elif fieldType == "StringField":
             f = StringField(label=key.replace("_", " "),
                         description=commentValue,
-                        default=str(fieldDefault),
+                        # default=str(fieldDefault),
                         validators=validators,
                         render_kw={'title':commentValue}
                         )
