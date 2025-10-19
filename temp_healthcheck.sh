309accfdc7180eeb3e1b9796f88939b81c7f90c1
1337server <sndspamfilter@gmail.com>
2025-10-19 18:28:28 +0100

    Changed build base image as we don't need such a large image if we are only running one service
    Removed comments from dockerfile
    
    broke ui requirements to its own file, to help track reqs separately
    
    Removed comments from health check, also udev isnt needed in ui container.

--- temp_healthcheck.sh
+++ temp_healthcheck.sh
@@ -1,5 +1,1 @@
 curl --fail "http://$(hostname -i):8080" || exit 1
-ps -e | pgrep systemd-udevd || exit 1
-#makemkvcon | grep www.makemkv.com/developers || exit 1
-#HandBrakeCLI --version || exit 1
-#abcde -v || exit 1
